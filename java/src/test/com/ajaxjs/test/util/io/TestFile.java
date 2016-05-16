8d2aa460e9a330d4e31eeb629439414827e919ab
frank42a <sp42@qq.com>
2016-05-16 08:43:34 -0400

    d

--- java/src/test/com/ajaxjs/test/util/io/TestFile.java
+++ java/src/test/com/ajaxjs/test/util/io/TestFile.java
@@ -8,27 +8,27 @@ import org.junit.Test;
 
 import com.ajaxjs.util.IO.text;
 import com.ajaxjs.util.Util;
- 
 
 public class TestFile {
 	@Test
-	public void testRead() throws IOException{
+	public void testRead() throws IOException {
 		String content = text.readFile(Util.getClassFolder_FilePath(TaskA.class, "taskconfig.xml"));
 		assertNotNull(content);
 		assertTrue(content.startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"));
 	}
+
 	@Test
-	public void testWrite() throws IOException{
+	public void testWrite() throws IOException {
 		String dir = TestFile.class.getResource("/").getPath();
 		String fullpath = dir + File.separator + "bar.txt";
 		System.out.println(fullpath);
 		text.save2file(fullpath, "bar");
 		// open it
-//		String d = Util.getClassFolder_FilePath(TestFile.class, "bar.txt");
-//		System.out.println(d);
+		// String d = Util.getClassFolder_FilePath(TestFile.class, "bar.txt");
+		// System.out.println(d);
 		String content = text.readFile(fullpath);
 		assertNotNull(content);
-		
+
 		Base.delete(fullpath);
 	}
 }
