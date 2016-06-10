1aee014bbbee87d5755da26b79177c30c1afa806
frank42a <sp42@qq.com>
2016-06-10 10:41:29 -0400

    d

---
+++ java-project/ajaxjs-es/src/test/com/ajaxjs/framework/service/TestBaseEsService.java
@@ -0,0 +1,26 @@
+package test.com.ajaxjs.framework.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import com.ajaxjs.framework.exception.ServiceException;
+import com.ajaxjs.framework.service.BaseEsService;
+
+import crawler.model.Video;
+
+
+
+public class TestBaseEsService {
+	@Test
+	public void testBaseESService() throws ServiceException {
+		BaseEsService<Video> news = new BaseEsService<>();
+		news.setReference(Video.class);
+		news.setIndex("dept");
+		news.setTableName("test");
+		Video video = news.getById(2539);
+		System.out.println(video.getName());
+		assertNotNull(video);
+		
+	}
+}
