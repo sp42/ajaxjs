b44cce08e87724b23c86715db4f04d5e34362844
frank42a <sp42@qq.com>
2016-07-03 12:49:56 -0400

    d

---
+++ java-project/ajaxjs-web/src/com/ajaxjs/framework/model/News.java
@@ -0,0 +1,7 @@
+package com.ajaxjs.framework.model;
+
+import com.ajaxjs.mvc.model.Entity;
+
+public class News extends Entity {
+
+}
--- java-project/ajaxjs-web/src/com/ajaxjs/mvc/view/PageResult.java
+++ java-project/ajaxjs-web/src/com/ajaxjs/mvc/view/PageResult.java
@@ -1,5 +1,19 @@
+/**
+ * Copyright 2015 Frank Cheung
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ajaxjs.mvc.view;
-
 
 import java.util.List;
 
@@ -7,9 +21,9 @@ import java.util.List;
  * 分页信息 bean
  * @author frank
  *
- * @param <T>
+ * @param <T> Bean 对象
  */
-public class PageResultView<T> {
+public class PageResult<T> {
 	private List<T> rows;	// 结果集
 	private int totalCount; // 总记录数
 	private int start; // 从第几笔记录开始
