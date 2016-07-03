38504bcc364c34d5153c7efe3f45f5b4e839ac9f
frank42a <sp42@qq.com>
2016-07-03 12:51:34 -0400

    d

---
+++ java-project/ajaxjs-web/src/com/ajaxjs/mvc/controller/ModelAndView.java
@@ -0,0 +1,21 @@
+package com.ajaxjs.mvc.controller;
+
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ModelAndView extends HashMap<String, Object> {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 保存到 request
+	 * 
+	 * @param request
+	 */
+	public void saveToReuqest(HttpServletRequest request) {
+		for (String key : keySet()) {
+			request.setAttribute(key.toString(), get(key));
+		}
+	}
+}
