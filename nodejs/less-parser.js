af9971ee3b8337403f3e6506f72e75b8387e221a
frank42a <sp42@qq.com>
2016-12-26 16:09:08 +0800

    修正 e 变量名的错误

--- nodejs/less-parser.js
+++ nodejs/less-parser.js
@@ -1,6 +1,7 @@
 const HTTP = require('http'), less = require('less'), fs = require('fs'), Step = require('./lib/step'), webUtils = require('./lib/webserver-utils');
 
-const port = 80, server = HTTP.createServer((req, res) => {
+const port = 80, base_less_folder = 'c:/project/ajaxjs/less',
+server = HTTP.createServer((req, res) => {
 	init(req, res);
 });
 
@@ -11,6 +12,7 @@ server.listen(port, null, () => {
 
 init = function (request, response) {
 	console.log('Current Less.js vresion:' + less.version.join('.'));
+	
 	var _req = webUtils.url2json(request.url),
 		lessFile = _req.lessFile,
 		ns = _req.ns; // 读取 url 的命名空间
@@ -20,8 +22,9 @@ init = function (request, response) {
 
 	// 指定包空间
 	var ns_arr = [
-		'c:/project/ajaxjs/less'
+	            base_less_folder
     ];
+	
 	ns && ns_arr.push(ns);
 
 	// 背景图的路径按 CSS 为基准。但因为 CSS 处于 Node 服务器上，所以要指定到 Tomcat 上才行。
@@ -34,8 +37,10 @@ init = function (request, response) {
 	}, function (err, lessFile) {
 		if (err) 
 			return webUtils.show500error(response, '未发行对应的样式文件！原因：' + err.toString());
+		
 		var isDebug = _req.isdebug; // 是否压缩样式 
 
+		
 		try {
 			less.render(assetFilePath + lessFile, {
 				paths: ns_arr,
@@ -46,24 +51,24 @@ init = function (request, response) {
 		}
 	}, function (err, tree) {
 		if (err) 
-			return webUtils.show500error(response, 'LESS 解析 less 失败！原因:' + e);
-		//console.log('parse:' + lessFile);
+			return webUtils.show500error(response, 'LESS 解析 less 失败！原因:' + err);
+		
 		response.writeHead(200, { 'Content-Type': 'text/css; charset=UTF-8' });
 
 		var isDebug = _req.isdebug; // 是否压缩样式
 
 		response.end(tree.css);
-		if (!isDebug || isDebug == 'false') { }
+		
+		if (!isDebug || isDebug == 'false') {}
 		else if (isDebug == 'true') { }
 
 		// 总是 localhost/app 后面有一个 app 项目名
-		//			deployUsed = deployUsed.replace(/http:\/\/localhost:8080\/\w+/i, _req.MainDomain);
-		//		console.log('--------' + tree);
+		// deployUsed = deployUsed.replace(/http:\/\/localhost:8080\/\w+/i, _req.MainDomain);
+		// console.log('--------' + tree);
 		console.log('--------' + _req.picPath);
 		var deployUsed = tree.css.replace(new RegExp(_req.picPath, 'g'), '../images');
 
-		// 保存文件
-		// 父级 css 目录下，save to file
+		// 保存文件在父级 css 目录下，save to file
 		// toCSS(lessFile.replace(/(.*?\/)(\w+)\.less/, '$1../css/$2.css'), deployUsed);
 	});
 }
@@ -89,16 +94,3 @@ function toCSS(newlessFile, css) {
 		fs.close(_fd);
 	});
 }
-
-function show500(res, msg) {
-	if (res) {
-		msg = msg || '读取 LESS 文件异常，原因未知！';
-		res.writeHead(500, { 'Content-Type': 'text/html;charset=UTF-8' });
-		res.end(msg);
-		console.log('------------------------------------------');
-		console.log('[Statics]' + new Date + msg);
-	} else {
-		console.log('Request 对象不存在！');
-	}
-}
-
