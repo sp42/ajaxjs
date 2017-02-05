e00e7fd6fcd1eef3dd74f98375d08119d2f3c664
frank42a <sp42@qq.com>
2017-02-05 21:47:21 +0800

    no commit message

--- ajaxjs-security/src/com/ajaxjs/web/security/ConfigLoader.java
+++ ajaxjs-security/src/com/ajaxjs/web/security/ConfigLoader.java
@@ -19,7 +19,6 @@ import java.io.IOException;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -49,19 +48,19 @@ public class ConfigLoader implements Filter {
 	private static boolean isEnableXSSFilter;
 	private static boolean isEnableCLRF_Filter;
 	private static boolean isEnableUploadFilter;
+	private static String redirectUrl;
 
-	/**
-	 * 读取配置，保存到 JVM 内存中
-	 */
 	@Override
 	public void init(FilterConfig config) throws ServletException {
 		System.out.println("安全框架启动中……");
+
+		redirectUrl = config.getInitParameter("redirectUrlt");
 
 		String encryCookieKey = config.getInitParameter("encryCookieKey");
 		if (encryCookieKey != null) {
 			if (encryCookieKey == null || encryCookieKey.length() != 16)
 				throw new ServletException("encrykey(" + encryCookieKey + ") 长度必须为 16");
-			
+
 			EncrySessionInCookie.key = encryCookieKey;
 		}
 
@@ -70,40 +69,39 @@ public class ConfigLoader implements Filter {
 			CookieRequest.delegate.whiteList = loadParams(cookieWhiteList);
 			CookieResponse.delegate.whiteList = CookieRequest.delegate.whiteList;
 		}
-		
+
 		String enableXSSFilter = config.getInitParameter("enableXSSFilter");
 		if (cookieWhiteList != null && "true".equals(enableXSSFilter)) {
 			isEnableXSSFilter = true;
 		}
-		
+
 		String enableCLRF_Filter = config.getInitParameter("enableCLRF_Filter");
 		if (enableCLRF_Filter != null && "true".equals(enableCLRF_Filter)) {
 			isEnableCLRF_Filter = true;
 		}
-		
+
 		String uploadfileWhiteList = config.getInitParameter("uploadfileWhiteList");
 		if (uploadfileWhiteList != null) {
 			UploadRequest.delegate.whiteList = loadParams(uploadfileWhiteList);
 		}
-		
+
 		String CSRF_Filter = config.getInitParameter("CSRF_Filter");
 		if (CSRF_Filter != null && "true".equals(CSRF_Filter)) {
-			
+
 		}
 	}
-	
 
-	private static boolean checkRedirectValid(String location) {
-		if (location == null || location.isEmpty())
-			return false;
-
-		for (Pattern pattern : ConfigLoader.redirectLocationWhiteList) {
-			if (pattern.matcher(location).find())
-				return true;
-		}
-
-		return false;
-	}
+	// private static boolean checkRedirectValid(String location) {
+	// if (location == null || location.isEmpty())
+	// return false;
+	//
+	// for (Pattern pattern : ConfigLoader.redirectLocationWhiteList) {
+	// if (pattern.matcher(location).find())
+	// return true;
+	// }
+	//
+	// return false;
+	// }
 
 	public String getCookieByName(String name, Cookie[] cookies) {
 		for (Cookie cookie : cookies) {
@@ -115,49 +113,54 @@ public class ConfigLoader implements Filter {
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
-		
-		if (CookieRequest.delegate.whiteList != null) {
-			CookieRequest cookieRequest = new CookieRequest(httpRequest);
-			httpRequest = (HttpServletRequest) cookieRequest;
-			
-			CookieResponse cookieResponse = new CookieResponse(httpResponse);
-			httpResponse = (HttpServletResponse)cookieResponse;
-		}
 
-		if(isEnableXSSFilter) {
-			XSS_Request xssRequest = new XSS_Request(httpRequest);
-			httpRequest = (HttpServletRequest) xssRequest;
-			
-			XSS_Response xssResponse = new XSS_Response(httpResponse);
-			httpResponse = (HttpServletResponse)xssResponse;
-		}
-		
-		if(isEnableCLRF_Filter) {
-			CLRF_Response CLRF_response = new CLRF_Response(httpResponse);
-			httpResponse = (HttpServletResponse)CLRF_response;
-		}
+		try {
+			if (CookieRequest.delegate.whiteList != null) {
+				CookieRequest cookieRequest = new CookieRequest(httpRequest);
+				httpRequest = (HttpServletRequest) cookieRequest;
 
-		if(isEnableUploadFilter) {
-			UploadRequest uploadRequest = new UploadRequest(httpRequest);
-			httpRequest = (HttpServletRequest)uploadRequest;
-		}
-		
-		if (EncrySessionInCookie.key != null) {
-			EncrySessionInCookie.deseriable(httpRequest, EncrySessionInCookie.key);
-		}
- 
-		filterChain.doFilter(httpRequest, new CLRF_Response(httpResponse));
+				CookieResponse cookieResponse = new CookieResponse(httpResponse);
+				httpResponse = (HttpServletResponse) cookieResponse;
+			}
 
-		if (EncrySessionInCookie.key != null) {
-			EncrySessionInCookie.seriable(httpRequest, httpResponse, EncrySessionInCookie.key);
+			if (isEnableXSSFilter) {
+				XSS_Request xssRequest = new XSS_Request(httpRequest);
+				httpRequest = (HttpServletRequest) xssRequest;
+
+				XSS_Response xssResponse = new XSS_Response(httpResponse);
+				httpResponse = (HttpServletResponse) xssResponse;
+			}
+
+			if (isEnableCLRF_Filter) {
+				CLRF_Response CLRF_response = new CLRF_Response(httpResponse);
+				httpResponse = (HttpServletResponse) CLRF_response;
+			}
+
+			if (isEnableUploadFilter) {
+				UploadRequest uploadRequest = new UploadRequest(httpRequest);
+				httpRequest = (HttpServletRequest) uploadRequest;
+			}
+
+			if (EncrySessionInCookie.key != null) {
+				EncrySessionInCookie.deseriable(httpRequest, EncrySessionInCookie.key);
+			}
+
+			filterChain.doFilter(httpRequest, new CLRF_Response(httpResponse));
+
+			if (EncrySessionInCookie.key != null) {
+				EncrySessionInCookie.seriable(httpRequest, httpResponse, EncrySessionInCookie.key);
+			}
+		} catch (SecurityException e) {
+			httpResponse.sendRedirect(redirectUrl + e);
 		}
 	}
 
-	private final static String URL_SPLIT_PATTERN = "[, ;\r\n]"; // 逗号  空格 分号 换行
-	
+	private final static String URL_SPLIT_PATTERN = "[, ;\r\n]"; // 逗号 空格 分号 换行
+
 	/**
 	 * 字符转换为 LIST
 	 * 
