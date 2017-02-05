86520cbc9afff65e9f82d602624be917999f246c
frank42a <sp42@qq.com>
2017-02-05 19:30:44 +0800

    no commit message

--- ajaxjs-security/src/com/ajaxjs/web/security/wrapper/XSS_Response.java
+++ ajaxjs-security/src/com/ajaxjs/web/security/wrapper/XSS_Response.java
@@ -15,13 +15,8 @@
  */
 package com.ajaxjs.web.security.wrapper;
 
-import java.io.IOException;
-import java.util.regex.Pattern;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
-
-import com.ajaxjs.web.security.ConfigLoader;
 
 /**
  * @author Frank
@@ -42,29 +37,10 @@ public class XSS_Response extends HttpServletResponseWrapper {
 		super.setHeader(name, XSS_Request.xssFilter(value, null));
 	}
 
-	@Override
-	public void sendRedirect(String location) throws IOException {
-		if (!checkRedirectValid(location)) {
-			throw new RuntimeException("跳转地址 " + location + " 非法，因为不在白名单中。");
-		}
-		super.sendRedirect(location);
-	}
-
+	// 对 Response 的 setStatus(int sc, String sm) 方法 sm 错误信息进行 XSS 过滤；
 	@SuppressWarnings("deprecation")
 	@Override
 	public void setStatus(int sc, String sm) {
 		super.setStatus(sc, XSS_Request.xssFilter(sm, null));
-	}
-
-	private static boolean checkRedirectValid(String location) {
-		if (location == null || location.isEmpty())
-			return false;
-
-		for (Pattern pattern : ConfigLoader.redirectLocationWhiteList) {
-			if (pattern.matcher(location).find())
-				return true;
-		}
-
-		return false;
 	}
 }
