5b8cb3c578b48b5bb85c622479a68efdd3b5c47b
frank42a <sp42@qq.com>
2017-02-28 10:46:26 -0500

    no commit message

--- ajaxjs-base/src/test/com/ajaxjs/jdbc/TestHelper.java
+++ ajaxjs-base/src/test/com/ajaxjs/jdbc/TestHelper.java
@@ -2,6 +2,7 @@ package test.com.ajaxjs.jdbc;
 
 import static org.junit.Assert.*;
 
+import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
@@ -17,38 +18,51 @@ import com.ajaxjs.util.Util;
 
 public class TestHelper {
 	Connection conn;
-	
-    @Before
-    public void setUp() {
-    	conn = JdbcConnection.getConnection("jdbc:sqlite:" + Util.getClassFolder_FilePath(TestSimpleORM.class, "foo.sqlite"));
-    }
-    
-    @After
-    public void setEnd() throws SQLException {
-    	conn.close();
-    }
+
+	@Before
+	public void setUp() throws SQLException {
+		conn = JdbcConnection.getConnection("jdbc:sqlite:" + Util.getClassFolder_FilePath(TestSimpleORM.class, "foo.sqlite"));
+		conn.setAutoCommit(true);
+	}
+
+	@After
+	public void setEnd() throws SQLException {
+		conn.close();
+	}
 
 	@Test
 	public void testConnection() throws SQLException {
 		assertNotNull(conn);
 	}
-	
+
 	@Test
-	public void testQuery(){
+	public void testQuery() {
 		assertNotNull(conn);
-		
+
 		Map<String, Object> info;
 		info = Helper.query(conn, "SELECT * FROM news WHERE id = 1");
-		// System.out.println(info);
 		assertNotNull(info);
-		
+
 		info = Helper.query(conn, "SELECT * FROM news WHERE id = ?", 1);
 		System.out.println(info.get("name"));
 		assertNotNull(info.get("name"));
-		
+
 		List<Map<String, Object>> newss = Helper.queryList(conn, "SELECT * FROM news");
 		assertNotNull(newss.get(0).get("name"));
 		System.out.println(newss.get(0).get("name"));
 	}
 
+	@Test
+	public void testCreateUpdate() throws SQLException {
+		Serializable newlyId = Helper.create(conn, "INSERT INTO news (name) VALUES (?)", "test2");
+		System.out.println(newlyId);
+		
+		int id = (int)newlyId;
+		assertNotNull(id);
+		
+		int updated;
+		updated = Helper.update(conn, "UPDATE news SET name = ? WHERE id = ?", "Hi", id);
+		updated = Helper.update(conn, "UPDATE news SET name = 'hi' WHERE id = 60");
+		assertEquals(1, updated);
+	}
 }
