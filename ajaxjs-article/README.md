93828c5c68a543b4e09a9f8fb7bd0321206ae8ec
frank42a <sp42@qq.com>
2017-05-12 22:33:42 +0800

    no commit message

--- ajaxjs-article/README.md
+++
@@ -1,3 +0,0 @@
-JSP 页面管理器
-=================================== 
-参见 ：http://blog.csdn.net/zhangxin09/article/details/51545128
--- ajaxjs-article/src/com/ajaxjs/article/model/Article.java
+++
@@ -1,7 +0,0 @@
-package com.ajaxjs.article.model;
-
-import com.ajaxjs.framework.model.Entity;
-
-public class Article extends Entity {
-
-}
