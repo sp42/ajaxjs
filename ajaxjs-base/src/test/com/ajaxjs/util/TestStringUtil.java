f3865cd51c6eb2ea494c58276972d22210923338
frank42a <sp42@qq.com>
2017-07-23 22:53:10 +0800

    no commit message

--- ajaxjs-base/src/test/com/ajaxjs/util/TestStringUtil.java
+++ ajaxjs-base/src/test/com/ajaxjs/util/TestStringUtil.java
@@ -1,16 +1,10 @@
 package test.com.ajaxjs.util;
 
 import static org.junit.Assert.*;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-
-import org.junit.Test;
-
 import static com.ajaxjs.util.StringUtil.*;
+
+import java.util.ArrayList;
+import org.junit.Test;
 
 public class TestStringUtil {
 	@Test
@@ -19,12 +13,11 @@ public class TestStringUtil {
 		assertTrue(isEmptyString(" "));
 		assertTrue(isEmptyString(null));
 	}
-	
+
 	@Test
 	public void testStringJoin() {
-		assertEquals(stringJoin(new String[]{"a", "b", "c"}, ","), "a,b,c");
-		assertEquals(stringJoin(new ArrayList<String>(){
-
+		assertEquals(stringJoin(new String[] { "a", "b", "c" }, ","), "a,b,c");
+		assertEquals(stringJoin(new ArrayList<String>() {
 			private static final long serialVersionUID = 1L;
 			{
 				add("a");
@@ -33,55 +26,33 @@ public class TestStringUtil {
 			}
 		}, ","), "a,b,c");
 	}
-	
+
 	@Test
 	public void testRepeatStr() {
 		assertEquals(repeatStr("Hi", ",", 3), "Hi,Hi,Hi");
-	}	
-	
+	}
+
 	@Test
 	public void testContainsIgnoreCase() {
 		assertTrue(containsIgnoreCase("abc", "A"));
-	}	
+	}
+
 	@Test
 	public void testRegMatch() {
-		assertEquals(regMatch("^a", "abc"), "a");
-		assertEquals(regMatch("^a", "abc", 0), "a");
+		assertEquals(regMatch("^a", "abc"), "a");// 匹配结果，只有匹配第一个
+		assertEquals(regMatch("^a", "abc", 0), "a");// 可指定分组
 		assertEquals(regMatch("^a(b)", "abc", 1), "b");
-	}	
-	
+	}
+
 	static String str = "中国";
-	
+
 	@Test
 	public void testUrlDecode() {
 		assertEquals(urlDecode(urlEncode(str)), str);
 	}
-	
-	public void testUTF8mb4() {
-		System.out.println("test string=" + "😄walmart öbama 👽💔");
-		String url = "jdbc:mysql://localhost:3306/foo?useUnicode=true&characterEncoding=UTF-8";
-		
-		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			Connection c = DriverManager.getConnection(url, "root", "123123");
-			c.prepareStatement("SET NAMES 'utf8mb4'").execute();
-			c.prepareStatement("insert into foo (name) values('😄walmart öbama 👽💔')").execute();
-			PreparedStatement p = c.prepareStatement("select * from foo");
-			p.execute();
-			ResultSet rs = p.getResultSet();
-			while (!rs.isLast()) {
-				rs.next();
-				String retrieved = rs.getString(1);
-				System.out.println("retrieved=\"" + retrieved + "\"");
-
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 	@Test
-	public void testMisc(){
+	public void testMisc() {
 		assertEquals(base64Decode(base64Encode(str)), str);
 		assertEquals(md5("123123"), "4297F44B13955235245B2497399D7A93");
 	}
