009eb51337288750378cb483d0c2c315823befe3
frank42a <sp42@qq.com>
2017-07-25 15:09:18 +0800

    no commit message

--- ajaxjs-base/src/test/com/ajaxjs/js/TestJSON2.java
+++ ajaxjs-base/src/test/com/ajaxjs/js/TestJSON2.java
@@ -1,4 +1,4 @@
-package test.com.ajaxjs.json;
+package test.com.ajaxjs.js;
 
 import static org.junit.Assert.*;
 
@@ -12,7 +12,7 @@ import com.ajaxjs.js.json.lexer.Lexer;
 import com.ajaxjs.js.json.lexer.Token;
 import com.ajaxjs.js.json.lexer.Tokens;
 
-public class TestJSON {
+public class TestJSON2 {
 	String str = "{foo:[],\n\ta:[1,-23333,-0.3,0.17,5.2,\"\\u82B1\\u6979~\"],\n\tb:[\"a\tbc\",\"12  3\",\"4,5\\\"6\",{\n\t\t\t\t\tx:1,\n\t\t\t\t\ty:\"cc\\ncc\"\n\t\t\t\t},4.56],\n\t\"text\":\"I'm OK~\",\n\t\"1-2\":234,\n\tmybool:false,\n\tmynull:null,\n\tmyreal:true\n}\n";
 	
 //	@Test
--- ajaxjs-base/src/test/com/ajaxjs/util/collection/TestMapHelper.java
+++ ajaxjs-base/src/test/com/ajaxjs/util/collection/TestMapHelper.java
@@ -11,13 +11,6 @@ import org.junit.Test;
 public class TestMapHelper {
 	@Test
 	public void testIsEmptyString() {
-		MapHelper<String, IValue> map = new MapHelper<String, IValue>();
-		map.put("foo", new Value("string"));
-		map.put("foo", new Value("123"));
-
-//		System.out.println(map.get("foo").getValueOf().getClass().getName());
-		assertNotNull(map.get("foo"));
-		assertEquals("java.lang.Integer", map.get("foo").getValueOf().getClass().getName());
 	}
 	
 	
