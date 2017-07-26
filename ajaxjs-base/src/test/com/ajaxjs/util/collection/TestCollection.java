c86a7a4f951a27187080f757a6c5ac2a9d07ac10
frank42a <sp42@qq.com>
2017-07-26 15:07:40 +0800

    no commit message

--- ajaxjs-base/src/test/com/ajaxjs/util/collection/TestCollection.java
+++ ajaxjs-base/src/test/com/ajaxjs/util/collection/TestCollection.java
@@ -3,36 +3,37 @@ package test.com.ajaxjs.util.collection;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import org.junit.Test;
 
-import com.ajaxjs.util.collection.CollectionUtil;
+import static com.ajaxjs.util.collection.CollectionUtil.*;
 
 public class TestCollection {
 	@Test
 	public void testIsNotNull() {
 		// Array
-		assertTrue(CollectionUtil.isNotNull(new Object[] { 1, 2 }));
-		assertTrue(CollectionUtil.isNotNull(new String[] { "1" }));
-		assertTrue(!CollectionUtil.isNotNull(new Object[] {}));
+		assertTrue(!isNull(new Object[] { 1, 2 }));
+		assertTrue(!isNull(new String[] { "1" }));
+		assertTrue(isNull(new Object[] {}));
 
 		Object[] arr = null;
 		String[] strs = null;
-		assertTrue(!CollectionUtil.isNotNull(arr));
-		assertTrue(!CollectionUtil.isNotNull(strs));
+		assertTrue(isNull(arr));
+		assertTrue(isNull(strs));
 
 		// List
-		assertTrue(CollectionUtil.isNotNull(new ArrayList<Object>() {
+		assertTrue(!isNull(new ArrayList<Object>() {
 			private static final long serialVersionUID = 1L;
 			{
 				add(1);
 			}
 		}));
 
-		assertTrue(!CollectionUtil.isNotNull(new ArrayList<String>()));
+		assertTrue(isNull(new ArrayList<String>()));
 
-		assertTrue(CollectionUtil.isNotNull(new ArrayList<Object>() {
+		assertTrue(!isNull(new ArrayList<Object>() {
 			private static final long serialVersionUID = 1L;
 			{
 				add(null);
@@ -40,13 +41,31 @@ public class TestCollection {
 		}));
 
 		// Map
-		assertTrue(CollectionUtil.isNotNull(new HashMap<String, Object>() {
+		assertTrue(!isNull(new HashMap<String, Object>() {
 			private static final long serialVersionUID = 1L;
 			{
 				put("foo", null);
 			}
 		}));
+
+		assertTrue(isNull(new HashMap<String, Object>()));
+	}
+
+	@Test
+	public void testArray() {
+		String str = "1,2,3";
+		int[] arr = new int[] { 1, 2, 3 };
 		
-		assertTrue(!CollectionUtil.isNotNull(new HashMap<String, Object>()));
+		assertEquals(Arrays.toString(strArr2intArr(str, ",")), Arrays.toString(arr));
+		assertEquals(Arrays.toString(int_arr2string_arr(arr)), Arrays.toString(new String[] { "1", "2", "3" }));
+		assertEquals(Arrays.toString(stringList2arr(new ArrayList<String>() {
+			private static final long serialVersionUID = 1L;
+			{
+				add("1");
+				add("2");
+				add("3");
+			}
+		})), Arrays.toString(new String[] { "1", "2", "3" }));
+
 	}
 }
