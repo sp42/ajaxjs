dfcad0a206a7fa519628ee87c8e7d9d47d64279d
frank42a <sp42@qq.com>
2017-12-22 20:54:05 +0800

    no commit message

--- src/main/com/ajaxjs/mvc/controller/IController.java
+++ src/main/com/ajaxjs/mvc/controller/IController.java
@@ -16,42 +16,42 @@
 package com.ajaxjs.mvc.controller;
 
 /**
- * 控制器
- * 一个 Controller 通常拥有多个方法，每个方法负责处理一个 URL。
- * 直接把一个 URL 映射到一个方法，这样，Web 开发人员就可以将多个功能类似的方法放到一个 Controller 中
+ * 控制器 一个 Controller 通常拥有多个方法，每个方法负责处理一个 URL。 直接把一个 URL 映射到一个方法，这样，Web
+ * 开发人员就可以将多个功能类似的方法放到一个 Controller 中
+ * 
  * @author Sp42 frank@ajaxjs.com
  */
 public interface IController {
 	/**
 	 * 操作成功，返回 msg 信息
 	 */
-	public static final String json_ok 		= "json::{\"isOk\": true, \"msg\" : \"%s\"}";
-	
+	public static final String json_ok = "json::{\"isOk\": true, \"msg\" : \"%s\"}";
+
 	/**
 	 * 操作失败，返回 msg 信息
 	 */
-	public static final String json_not_ok 	= "json::{\"isOk\": false, \"msg\" : \"%s\"}";
+	public static final String json_not_ok = "json::{\"isOk\": false, \"msg\" : \"%s\"}";
 
 	/**
 	 * 全局 json 模板路径 之前缀
 	 */
 	public static final String jsp_perfix = "/asset/jsp/";
-	
+
 	/**
 	 * 常见 jsp 页面
 	 */
 	public static final String common_jsp_perfix = "/asset/common/jsp/";
-	
+
 	/**
 	 * json 路径常量（实体创建、修改）
 	 */
 	public static final String cud = common_jsp_perfix + "json/json-cud.jsp";
-	
+
 	/**
 	 * json 路径常量（实体）
 	 */
 	public static final String show_json_info = common_jsp_perfix + "json/showInfo.jsp";
-	
+
 	/**
 	 * json 路径常量（分页列表）
 	 */
@@ -60,14 +60,14 @@ public interface IController {
 	/**
 	 * 实体 jsp 模版路径之前缀
 	 */
-	public static final String jsp_list		 = jsp_perfix + "%s/list";
-	
+	public static final String jsp_list = jsp_perfix + "%s/list";
+
 	public static final String jsp_adminList = jsp_perfix + "%s/admin-list";
-	
-	public static final String jsp_info 	 = jsp_perfix + "%s/info";
-	
+
+	public static final String jsp_info = jsp_perfix + "%s/info";
+
 	public static final String jsp_adminInfo = jsp_perfix + "%s/admin-info";
-	
+
 	/**
 	 * 保存错误信息的 key
 	 */
