85760e9ea309cd7f1f6a2c37acacc8088519cd41
frank42a <sp42@qq.com>
2018-01-20 18:28:37 +0800

    no commit message

--- src/test/test/com/ajaxjs/mvc/TestQuery.java
+++ src/test/test/com/ajaxjs/mvc/TestQuery.java
@@ -17,19 +17,17 @@ public class TestQuery {
 	@Test
 	public void testQuery() {
 		HttpServletRequest request = mock(HttpServletRequest.class);
-//		assertFalse(Query.isAnyMatch(request.getParameterMap()));
-//
-//		when(request.getParameter("filterField")).thenReturn("name");
-//		when(request.getParameter("filterValue")).thenReturn("jack");
-//		assertTrue(Query.isAnyMatch(request.getParameterMap()));
-//
-//		Map<String, String[]> mock = new HashMap<>();
-//		mock.put("filterField", new String[] { "name" });
-//		mock.put("filterValue", new String[] { "jack" });
-//
-//		when(request.getParameterMap()).thenReturn(mock);
-//		QueryParams query = QueryParams.getQueryFactory(request.getParameterMap());
-//
-//		assertNotNull(query);
+		when(request.getParameter("filterField")).thenReturn("name");
+		when(request.getParameter("filterValue")).thenReturn("jack");
+
+		Map<String, String[]> mock = new HashMap<>();
+		mock.put("filterField", new String[] { "name" });
+		mock.put("filterValue", new String[] { "jack" });
+
+		when(request.getParameterMap()).thenReturn(mock);
+
+		QueryParams q = new QueryParams(request.getParameterMap());
+
+		assertEquals(" WHERE name = jack", q.addWhereToSql(""));
 	}
 }
