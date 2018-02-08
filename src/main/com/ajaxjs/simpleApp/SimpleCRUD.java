e4b5f62c12ee7dd07e64377d83702ea833dc8504
frank42a <sp42@qq.com>
2018-02-08 17:08:24 +0800

    no commit message

--- src/main/com/ajaxjs/simpleApp/SimpleCRUD.java
+++
@@ -1,5 +0,0 @@
-package com.ajaxjs.simpleApp;
-
-public class SimpleCRUD {
-//	return common_jsp_perfix + "simple_admin/edit-single-entry";
-}
--- src/main/com/ajaxjs/simpleApp/service/DataDictService.java
+++
@@ -1,9 +0,0 @@
-package com.ajaxjs.simpleApp.service;
-
-import java.util.Map;
-
-import com.ajaxjs.framework.service.IService;
-
-public interface DataDictService extends IService<Map<String, Object>, Integer> {
-
-}
