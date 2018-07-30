fde45ee4349f72c2114df6b89f7a0a924bea8538
Clare <18719139566@163.com>
2018-07-31 00:24:23 +0800

    解决冲突，pc 增加javax.servlet-api依赖

--- src/test/test/com/ajaxjs/mvc/controller/testcase/TestRequestParam.java
+++ src/test/test/com/ajaxjs/mvc/controller/testcase/TestRequestParam.java
@@ -1,40 +1,35 @@
 package test.com.ajaxjs.mvc.controller.testcase;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
-
-import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
-
-import org.junit.Test;
-import org.mockito.Mockito;
-
 import com.ajaxjs.mvc.ModelAndView;
 import com.ajaxjs.mvc.controller.ControllerScanner;
 import com.ajaxjs.mvc.controller.IController;
 import com.ajaxjs.mvc.controller.MvcRequest;
 import com.ajaxjs.mvc.controller.RequestParam;
 import com.ajaxjs.web.MockWeb;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.*;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+//import test.com.ajaxjs.framework.testcase.News;
 
 public class TestRequestParam {
-	
+
 	@Path("/foo")
 	public static class c1 implements IController {
 		@GET
 		public String get(Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
 			return "get";
 		}
-		
+
 		@POST
 		public String get2(ModelAndView mv, News news) {
 			return "get";
@@ -45,7 +40,7 @@ public class TestRequestParam {
 		public String getInfo2(@QueryParam("name") String name, @QueryParam("sex") String sex) {
 			return "getInfo2";
 		}
-		
+
 		@Path("/bar")
 		@POST
 		public String getInfo3(@QueryParam("sex") String sex) {
@@ -58,50 +53,50 @@ public class TestRequestParam {
 			return "getInfo2";
 		}
 	}
-	
+
 	static Map<String, String[]> map = new HashMap<>();
-	
-	
+
+
 	@Test
 	public void testGetArgs() {
 		ControllerScanner.add(c1.class);
-		
+
 		HttpServletRequest request = MockWeb.mockRequest("/ajaxjs-web", "/combo/person");
-		
+
 		map.put("foo", new String[] {"bar"});
 		when(request.getParameterMap()).thenReturn(map);
 
 		HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
 		StringWriter writer = MockWeb.writerFactory(response);
-		
+
 		Object[] args = RequestParam.getArgs(new MvcRequest(request), response, ControllerScanner.urlMappingTree.get("foo").getMethod);
 		assertNotNull(writer.toString());
-		
+
 		assertTrue(args[0] instanceof Map);
 		assertTrue(args[1] instanceof HttpServletRequest);
 		assertTrue(args[2] instanceof HttpServletResponse);
-		
+
 		args = RequestParam.getArgs(new MvcRequest(request), response, ControllerScanner.urlMappingTree.get("foo").postMethod);
 		assertTrue(args[0] instanceof ModelAndView);
 		assertTrue(args[1] instanceof News);
 	}
-	
+
 	@Test
 	public void testGetArgsValue() {
 		HttpServletRequest request = MockWeb.mockRequest("/ajaxjs-web", "/foo/12888/");
 		HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
-		
+
 		when(request.getParameter("name")).thenReturn("foo");
 		when(request.getParameter("sex")).thenReturn("bar");
-		
+
 		ControllerScanner.add(c1.class);
 		Object[] args = RequestParam.getArgs(new MvcRequest(request), response, ControllerScanner.urlMappingTree.get("foo").children.get("bar").getMethod);
 		assertEquals("foo", args[0]);
 		assertEquals("bar", args[1]);
-		
+
 		args = RequestParam.getArgs(new MvcRequest(request), response, ControllerScanner.urlMappingTree.get("foo").children.get("bar").postMethod);
 		assertEquals("bar", args[0]);
-		
+
 		args = RequestParam.getArgs(new MvcRequest(request), response, ControllerScanner.urlMappingTree.get("foo").children.get("{id}").putMethod);
 		assertEquals("12888", args[0]);
 	}
