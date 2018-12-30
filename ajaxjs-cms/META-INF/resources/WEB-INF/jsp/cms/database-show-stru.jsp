5880770d44edaecbf54e62c5c8bd0fc0e6720994
frank42a <sp42@qq.com>
2018-12-30 21:06:05 +0800

    no commit message

--- ajaxjs-cms/META-INF/resources/WEB-INF/jsp/cms/database-show-stru.jsp
+++ ajaxjs-cms/META-INF/resources/WEB-INF/jsp/cms/database-show-stru.jsp
@@ -1,6 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	import="java.util.*, java.sql.*, com.ajaxjs.cms.controller.DataBaseShowStruController, com.ajaxjs.mvc.filter.DataBaseFilter,com.ajaxjs.orm.JdbcConnection"
 	pageEncoding="UTF-8"%>
+<%@taglib uri="/ajaxjs" prefix="c"%>
+<%
+	DataBaseFilter.initDb();
+	Connection conn = JdbcConnection.getConnection();
+	
+	List<String> tables = DataBaseShowStruController.getAllTableName(conn);
+	Map<String, String> tablesComment = DataBaseShowStruController.getCommentByTableName(conn, tables);
+	Map<String, List<Map<String, String>>> infos = DataBaseShowStruController.getColumnCommentByTableName(conn, tables);
+%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,56 +21,19 @@
 	<div class="leftSide">
 		<br /> <br /> <br />
 		<h3>${aj_allConfig.site.titlePrefix} 表设计文档</h3>
-		<h5>2017.6.20 更新</h5>
+		<h5></h5>
 		<menu>
 			<ol class="level_1">
-
-				<li>
-					<h4>用户</h4>
-					<ol>
-						<li><a href="#user">用户信息表</a></li>
-						<li><a href="#user_common_auth">用户普通口令登录</a></li>
-						<li><a href="#user_oauth">用户第三方登录</a></li>
-						<li><a href="#user_login_log">用户登录日志</a></li>
-					</ol>
-				</li>
-				<li>
-					<h4>实体类型</h4>
-					<ol>
-						<li><a href="#article">文章</a></li>
-						<li><a href="#album">专辑</a></li>
-						<li><a href="#topic">专题</a></li>
-						<li><a href="#vod">视频点播</a></li>
-						<li><a href="#album_pic">相册</a></li>
-					</ol>
-				</li>
-				<li>
-					<h4>栏目</h4>
-					<ol>
-						<li><a href="#section_info">栏目信息</a></li>
-						<li><a href="#section_content">栏目列表</a></li>
-					</ol>
-				</li>
-				<li>
-					<h4>系统通用表</h4>
-					<ol>
-						<li><a href="#general_catelog">分类</a></li>
-						<li><a href="#general_data_dict">数据字典</a></li>
-						<li><a href="#general_log">全局操作日志</a></li>
-						<li><a href="#attachment">附件</a></li>
-						<li><a href="#attachment_picture">附件：图片</a></li>
-					</ol>
-				</li>
-
+			<c:foreach items="<%=tables%>" var="item">
+				<li><a href="#${item}">${item}</a></li>
+			</c:foreach>
 			</ol>
 		</menu>
-
 
 		<div
 			style="height: 30px; width: 82%; margin: 0 5%; overflow: hidden; line-height: 30px; white-space: nowrap; font-size: .9rem; color: gray; position: relative;">
 			<div class="mask"></div>
-			<div class="m" style="width: 100%; overflow: hidden;">Powered
-				by AJAXJS Framework!</div>
+			<div class="m" style="width: 100%; overflow: hidden;">Powered by AJAXJS Framework!</div>
 			<script>
 				window.setInterval(function() { // 跑马灯
 					var m = document.querySelector('.m');
@@ -74,13 +46,6 @@
 	</div>
 	<div class="rightSide">
 		<%
-			DataBaseFilter.initDb();
-			Connection conn = JdbcConnection.getConnection();
-		
-			List<String> tables = DataBaseShowStruController.getAllTableName(conn);
-			Map<String, String> tablesComment = DataBaseShowStruController.getCommentByTableName(conn, tables);
-			Map<String, List<Map<String, String>>> infos = DataBaseShowStruController.getColumnCommentByTableName(conn, tables);
-
 			for (String tableName : tables) {
 				if ("template".equals(tableName))
 					continue;
@@ -97,10 +62,10 @@
 				<th>允许为空</th>
 				<th>字段说明</th>
 			</thead>
-			<%
-				for (Map<String, String> i : info) {
-						request.setAttribute("info", i);
-			%>
+		<%
+			for (Map<String, String> i : info) {
+					request.setAttribute("info", i);
+		%>
 			<tr>
 				<td width="20%" style="text-align: left; padding-left: 3%;">${info.name}</td>
 				<td width="15%">${info.type.toUpperCase()}</td>
@@ -108,12 +73,12 @@
 				<td style="text-align: left;">${info.comment}</td>
 			</tr>
 
-			<%
-				}
-			%>
+		<%
+			}
+		%>
 
 		</table>
-			<%
+		<%
 			}
 			JdbcConnection.closeDb();
 			JdbcConnection.clean();
--- ajaxjs-cms/src/main/java/com/ajaxjs/cms/controller/DataBaseShowStruController.java
+++ ajaxjs-cms/src/main/java/com/ajaxjs/cms/controller/DataBaseShowStruController.java
@@ -16,12 +16,13 @@ import javax.ws.rs.Path;
 import com.ajaxjs.framework.BaseController;
 import com.ajaxjs.mvc.ModelAndView;
 import com.ajaxjs.mvc.controller.IController;
+import com.ajaxjs.orm.JdbcHelper;
 
 @Path("/admin/DataBaseShowStru")
 public class DataBaseShowStruController implements IController {
 	@GET
 	public String show(ModelAndView mv, HttpServletRequest request) {
-		return  BaseController.cms("database-show-stru");
+		return BaseController.cms("database-show-stru");
 	}
 
 	/**
@@ -36,7 +37,6 @@ public class DataBaseShowStruController implements IController {
 				String createDDL = rs.getString(2);
 				return parse(createDDL);
 			}
-
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -53,19 +53,22 @@ public class DataBaseShowStruController implements IController {
 	public static Map<String, String> getCommentByTableName(Connection conn, List<String> tableNames) {
 		Map<String, String> map = new HashMap<>();
 
-		try (Statement stmt = conn.createStatement();) {
+		JdbcHelper.stmt(conn, stmt -> {
 			for (String tableName : tableNames) {
-				try (ResultSet rs = stmt.executeQuery("SHOW CREATE TABLE " + tableName);) {
-					if (rs != null && rs.next()) {
-						String createDDL = rs.getString(2);
-						String comment = parse(createDDL);
-						map.put(tableName, comment);
+				JdbcHelper.rsHandle(stmt, "SHOW CREATE TABLE " + tableName, rs -> {
+					String createDDL = null;
+					try {
+						if (rs.next())
+							createDDL = rs.getString(2);
+					} catch (SQLException e) {
+						e.printStackTrace();
 					}
-				}
+
+					String comment = parse(createDDL);
+					map.put(tableName, comment);
+				});
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		});
 
 		return map;
 	}
@@ -77,6 +80,8 @@ public class DataBaseShowStruController implements IController {
 	 * @return
 	 */
 	private static String parse(String all) {
+		if (all == null)
+			return null;
 		String comment = null;
 		int index = all.indexOf("COMMENT='");
 		if (index < 0)
@@ -94,12 +99,15 @@ public class DataBaseShowStruController implements IController {
 	 */
 	public static List<String> getAllTableName(Connection conn) {
 		List<String> tables = new ArrayList<>();
-		try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery("SHOW TABLES ");) {
-			while (rs.next())
-				tables.add(rs.getString(1));
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+
+		JdbcHelper.query(conn, "SHOW TABLES", rs -> {
+			try {
+				while (rs.next())
+					tables.add(rs.getString(1));
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		});
 
 		return tables;
 	}
@@ -107,26 +115,15 @@ public class DataBaseShowStruController implements IController {
 	public static Map<String, List<Map<String, String>>> getColumnCommentByTableName(Connection conn, List<String> tableNames) {
 		Map<String, List<Map<String, String>>> map = new HashMap<>();
 
-		try (Statement stmt = conn.createStatement();) {
+		JdbcHelper.stmt(conn, stmt -> {
 			for (String tableName : tableNames) {
-				try (ResultSet rs = stmt.executeQuery("show full columns from " + tableName);) {
+				JdbcHelper.rsHandle(stmt, "SHOW FULL COLUMNS FROM " + tableName, rs -> {
 					List<Map<String, String>> list = new ArrayList<>();
-
-					while (rs.next()) {
-						Map<String, String> fieldInfos = new HashMap<>();
-						fieldInfos.put("name", rs.getString("Field"));
-						fieldInfos.put("type", rs.getString("Type"));
-						fieldInfos.put("null", rs.getString("Null"));
-						fieldInfos.put("comment", rs.getString("Comment"));
-						list.add(fieldInfos);
-					}
-
+					rs2list(rs, list);
 					map.put(tableName, list);
-				}
+				});
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		});
 
 		return map;
 	}
@@ -134,24 +131,25 @@ public class DataBaseShowStruController implements IController {
 	public static List<Map<String, String>> getColumnCommentByTableName(Connection conn, String tableName) {
 		List<Map<String, String>> list = new ArrayList<>();
 
-		try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery("show full columns from " + tableName);) {
+		JdbcHelper.query(conn, "SHOW FULL COLUMNS FROM " + tableName, rs -> {
 			rs2list(rs, list);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		});
 
 		return list;
 	}
 
-	private static void rs2list(ResultSet rs, List<Map<String, String>> list) throws SQLException {
-		while (rs.next()) {
-			Map<String, String> fieldInfos = new HashMap<>();
-			fieldInfos.put("name", rs.getString("Field"));
-			
-			fieldInfos.put("type", rs.getString("Type"));
-			fieldInfos.put("null", rs.getString("Null"));
-			fieldInfos.put("comment", rs.getString("Comment"));
-			list.add(fieldInfos);
+	private static void rs2list(ResultSet rs, List<Map<String, String>> list) {
+		try {
+			while (rs.next()) {
+				Map<String, String> fieldInfos = new HashMap<>();
+				fieldInfos.put("name", rs.getString("Field"));
+				fieldInfos.put("type", rs.getString("Type"));
+				fieldInfos.put("null", rs.getString("Null"));
+				fieldInfos.put("comment", rs.getString("Comment"));
+				list.add(fieldInfos);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 }
