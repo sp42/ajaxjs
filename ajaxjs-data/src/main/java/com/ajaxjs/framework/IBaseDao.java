dd0d0cab37974220deb676b1d3050d44ddef4194
frank42a <sp42@qq.com>
2019-02-18 09:29:18 +0800

    no commit message

--- ajaxjs-data/src/main/java/com/ajaxjs/framework/IBaseDao.java
+++ ajaxjs-data/src/main/java/com/ajaxjs/framework/IBaseDao.java
@@ -176,7 +176,7 @@ public interface IBaseDao<T> {
 			countSql = 	"SELECT COUNT(entry.id) AS count FROM ${tableName} entry WHERE catelogId IN " + catelog_find + " AND 1 = 1",
 
 			sqliteValue = "SELECT id, name, createDate, updateDate, entry.catelogId, catelogName FROM ${tableName} entry INNER JOIN "
-					   		+ catelog_finById_sqlite + " ON entry.`catelogId` = c.catelogId  WHERE 1 = 1",
+					   		+ catelog_finById_sqlite + " ON entry.`catelogId` = c.catelogId  WHERE 1 = 1 ORDER BY id DESC",
 			sqliteCountSql = "SELECT COUNT(entry.id) AS count FROM ${tableName} entry WHERE catelogId IN " + catelog_find_sqlite + " AND 1 = 1")
 	public PageResult<T> findPagedListByCatelogId(int catelogId,  int start, int limit);
 	
