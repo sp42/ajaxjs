1753ac70cde4fe8b4a1cdaf9af1a83f50380054b
frank42a <sp42@qq.com>
2019-06-25 00:55:36 +0800

    no commit message

--- ajaxjs-user/src/main/java/com/ajaxjs/user/common/UserAdminFilter.java
+++ ajaxjs-user/src/main/java/com/ajaxjs/user/common/UserAdminFilter.java
@@ -1,4 +1,4 @@
-package com.ajaxjs.cms.controller;
+package com.ajaxjs.user.common;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -17,7 +17,7 @@ import com.ajaxjs.framework.BaseController;
 /**
  * 进入后台一切资料的拦截器
  */
-public class AdminFilter implements Filter {
+public class UserAdminFilter implements Filter {
 	@SuppressWarnings("deprecation")
 	@Override
 	public void doFilter(ServletRequest _request, ServletResponse _response, FilterChain chain) throws IOException, ServletException {
@@ -25,7 +25,8 @@ public class AdminFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) _response;
 
 		if (request.getRequestURI().equals(request.getContextPath() + "/admin/login/")) {
-			request.getRequestDispatcher(BaseController.jsp("user/admin-login.jsp")).forward(request, response);
+			// 后台登录
+			request.getRequestDispatcher(BaseController.jsp("user/admin/admin-login.jsp")).forward(request, response);
 		} else if (request.getSession().getAttribute("userId") == null) {
 			response.setStatus(401, "Authentication Required");
 			response.setCharacterEncoding(StandardCharsets.UTF_8.toString());
