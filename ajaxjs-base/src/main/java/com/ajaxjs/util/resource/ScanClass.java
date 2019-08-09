6d83fa10904c1d15f798b0230e66365729a8b443
frank42a <sp42@qq.com>
2019-08-09 20:42:44 +0800

    no commit message

--- ajaxjs-base/src/main/java/com/ajaxjs/util/resource/ScanClass.java
+++ ajaxjs-base/src/main/java/com/ajaxjs/util/resource/ScanClass.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ajaxjs.util.io.resource;
+package com.ajaxjs.util.resource;
 
 import java.io.File;
 import java.io.FileFilter;
