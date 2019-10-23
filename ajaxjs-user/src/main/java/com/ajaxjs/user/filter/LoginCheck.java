bf7cb45e36df0a0b90afc61d85cc894fc4751fcf
frank42a <sp42@qq.com>
2019-10-23 22:30:04 +0800

    no commit message

---
+++ ajaxjs-user/src/main/java/com/ajaxjs/user/filter/LoginCheck.java
@@ -0,0 +1,30 @@
+package com.ajaxjs.user.filter;
+
+import java.lang.reflect.Method;
+
+import com.ajaxjs.mvc.controller.MvcOutput;
+import com.ajaxjs.mvc.controller.MvcRequest;
+import com.ajaxjs.mvc.filter.FilterAction;
+import com.ajaxjs.user.controller.BaseUserController;
+
+/**
+ * 是否已经登录的拦截器
+ * 
+ * @author sp42 frank@ajaxjs.com
+ *
+ */
+public class LoginCheck implements FilterAction {
+
+	@Override
+	public boolean before(MvcRequest request, MvcOutput response, Method method) {
+		if (BaseUserController.isLogined(request)) {
+			return true;
+		} else {
+			throw new IllegalAccessError("尚未登录，没有权限操作");
+		}
+	}
+
+	@Override
+	public void after(MvcRequest request, MvcOutput response, Method method, boolean isSkip) {
+	}
+}
