93e557987e5df55ffe12b528c10dfb4e68840906
frank42a <sp42@qq.com>
2020-01-07 14:20:14 +0800

    no commit message

--- ajaxjs-base/src/test/java/com/ajaxjs/net/TestMail.java
+++ ajaxjs-base/src/test/java/com/ajaxjs/net/TestMail.java
@@ -13,17 +13,33 @@ import com.ajaxjs.net.mail.Mail;
 import com.ajaxjs.net.mail.Sender;
 
 public class TestMail {
-	@Test
+//	@Test
 	public void test163() {
 		Mail mail = new Mail();
 		mail.setMailServer("smtp.163.com");
 		mail.setAccount("pacoweb");
+		mail.setPassword("abc123");
 		mail.setFrom("pacoweb@163.com");
-		mail.setTo("sp42@qq.com");
-		mail.setSubject("hihi44你好");
-		mail.setHTML_body(true);
-		mail.setContent("dsfds放到沙发dfsfd<a href=\"http://qq.com\">fdsfds</a>");
+		mail.setTo("frank@ajaxjs.com");
+		mail.setSubject("你好容祯");
+		mail.setHTML_body(false);
+		mail.setContent("xccx我希望可以跟你做朋友34354344");
 
+		assertTrue(Sender.send(mail));
+	}
+	
+	@Test
+	public void testYM163() {
+		Mail mail = new Mail();
+		mail.setMailServer("smtp.ym.163.com");
+		mail.setAccount("admin@bgdiving.com");
+		mail.setPassword("bgdiving123123");
+		mail.setFrom("admin@bgdiving.com");
+		mail.setTo("frank@ajaxjs.com");
+		mail.setSubject("你好容祯");
+		mail.setHTML_body(false);
+		mail.setContent("xccx我希望可以跟你做朋友34354344");
+		
 		assertTrue(Sender.send(mail));
 	}
 	
