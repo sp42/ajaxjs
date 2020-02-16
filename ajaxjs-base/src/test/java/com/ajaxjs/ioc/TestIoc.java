ff3033c4d3561241c3e21de8eb17eb6a2efa81b9
frank42a <sp42@qq.com>
2020-02-16 14:26:30 +0800

    no commit message

--- ajaxjs-base/src/test/java/com/ajaxjs/ioc/TestIoc.java
+++ ajaxjs-base/src/test/java/com/ajaxjs/ioc/TestIoc.java
@@ -51,6 +51,7 @@ public class TestIoc {
 		assertNotNull(hi);
 
 		Person person = (Person) BeanContext.getBean("person");
+		assertNotNull(person);
 		assertEquals("Hello Rose", hi.sayHello());
 	}
 
