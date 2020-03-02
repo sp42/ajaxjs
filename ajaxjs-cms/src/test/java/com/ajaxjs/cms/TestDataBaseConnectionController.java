64e00385ed2b6ba996e4b51eb6f99622a1e7aafb
frank42a <sp42@qq.com>
2020-03-02 10:04:38 +0800

    no commit message

--- ajaxjs-cms/src/test/java/com/ajaxjs/cms/TestDataBaseConnectionController.java
+++ ajaxjs-cms/src/test/java/com/ajaxjs/cms/TestDataBaseConnectionController.java
@@ -7,13 +7,12 @@ import java.util.Map;
 
 import org.junit.Test;
 
-import com.ajaxjs.cms.controller.DataBaseController;
-
+import com.ajaxjs.cms.controller.DataBaseShowStruController;
 
 public class TestDataBaseConnectionController {
 	@Test
 	public void testService() {
-		List<Map<String, String>> list = DataBaseController.get("C:\\project\\zyjf_admin\\WebContent\\META-INF\\context.xml");
+		List<Map<String, String>> list = DataBaseShowStruController.getConnectionConfig("C:\\project\\zyjf_admin\\WebContent\\META-INF\\context.xml");
 		
 		assertEquals("jdbc/mysql", list.get(0).get("name"));
 	}
