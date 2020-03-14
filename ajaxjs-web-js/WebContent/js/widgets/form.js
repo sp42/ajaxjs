21379bde65931669047989e043f2f6313c5dfc0f
frank42a <sp42@qq.com>
2020-03-14 13:13:39 +0800

    no commit message

--- ajaxjs-web-js/WebContent/js/widgets/form.js
+++ ajaxjs-web-js/WebContent/js/widgets/form.js
@@ -408,7 +408,7 @@ Vue.component('aj-form-html-editor', {
 			this.iframeDoc.body.innerHTML = html;
 		},
 		
-		saveRemoteImage2Local(){
+		saveRemoteImage2Local() {
 			var str = [], remotePicArr = [], arr = this.iframeDoc.querySelectorAll('img');
 
 			for(var i = 0, j = arr.length; i <j; i++) {
@@ -556,63 +556,112 @@ Vue.component('aj-form-html-editor', {
 			
 			return h;
 		},
-		// TODO https://github.com/jitbit/HtmlSanitizer/blob/master/HtmlSanitizer.js
 		// 清理冗余 HTML
 		cleanHTML(){
-		    // 类似于 白名单
-		    var tagsAllowed = "|h1|h2|h3|p|div|a|b|strong|br|ol|ul|li|pre|img|br|hr|font|";
-
-		    var attributesAllowed = {};
-		    attributesAllowed["div"] = "|id|class|";
-		    attributesAllowed["a"] = "|id|class|href|name|";
-		    attributesAllowed["img"] = "|src|";
-
-		    this.everyNode(this.iframeBody, node => {
-		    	var isDelete = false;
-		    	
-		    	if (node.nodeType === 1) {
-		    		var tag = node.tagName.toLowerCase();
-		    		if (tagsAllowed.indexOf("|" + tag + "|") === -1)
-		    			isDelete = true;
-		    		
-		    		if (!isDelete) { // 删除属性
-		    			var attrs = node.attributes;
-
-		    			for(var i = attrs.length - 1; i >= 0; i--) {
-		    				var name = attrs[i].name;
-		    				if (attributesAllowed[tag] == null || attributesAllowed[tag].indexOf("|" + name.toLowerCase() + "|") == -1){
-		    					node.removeAttribute(name);
-		    				}
-		    			}
-		    		}
-		    	} else if (node.nodeType === 8) {// 删除注释
-		    		isDelete = true;
-		    	}
-		    	
-		    	return isDelete;
-		    });
-
-		},
-		// 遍历节点
-	    everyNode (el, fn) {
-	        var objChildNode = el.firstChild;
-	        while (objChildNode) {
-	            if (fn(objChildNode)) { // 返回 true 则删除
-	                var next = objChildNode.nextSibling;
-	                el.removeChild(objChildNode);
-	                objChildNode = next;
-	            } else {
-	                if (objChildNode.nodeType === 1) 
-	                    this.everyNode(objChildNode, fn);
-
-	                objChildNode = objChildNode.nextSibling;
-	            }
-	        }
-	    }
+		    this.iframeBody.innerHTML = HtmlSanitizer.SanitizeHtml(this.iframeBody.innerHTML);
+		}
 	}
 });
 
+// https://github.com/jitbit/HtmlSanitizer/blob/master/HtmlSanitizer.js
+var HtmlSanitizer = new (function () {
+	var tagWhitelist_ = {
+		'A': true, 'ABBR': true, 'B': true, 'BLOCKQUOTE': true, 'BODY': true, 'BR': true, 'CENTER': true, 'CODE': true, 'DIV': true, 'EM': true, 'FONT': true,
+		'H1': true, 'H2': true, 'H3': true, 'H4': true, 'H5': true, 'H6': true, 'HR': true, 'I': true, 'IMG': true, 'LABEL': true, 'LI': true, 'OL': true, 'P': true, 'PRE': true,
+		'SMALL': true, 'SOURCE': true, 'SPAN': true, 'STRONG': true, 'TABLE': true, 'TBODY': true, 'TR': true, 'TD': true, 'TH': true, 'THEAD': true, 'UL': true, 'U': true, 'VIDEO': true
+	};
 
+	var contentTagWhiteList_ = { 'FORM': true }; //tags that will be converted to DIVs
+	var attributeWhitelist_ = { 'align': true, 'color': true, 'controls': true, 'height': true, 'href': true, 'src': true, 'style': true, 'target': true, 'title': true, 'type': true, 'width': true };
+	var cssWhitelist_ = { 'color': true, 'background-color': true, 'font-size': true, 'text-align': true, 'text-decoration': true, 'font-weight': true };
+	var schemaWhiteList_ = [ 'http:', 'https:', 'data:', 'm-files:', 'file:', 'ftp:' ]; //which "protocols" are allowed in "href", "src" etc
+	var uriAttributes_ = { 'href': true, 'action': true };
+
+	this.SanitizeHtml = function(input) {
+		input = input.trim();
+		if (input == "") return ""; //to save performance and not create iframe
+
+		//firefox "bogus node" workaround
+		if (input == "<br>") return "";
+
+		var iframe = document.createElement('iframe');
+		if (iframe['sandbox'] === undefined) {
+			alert('Your browser does not support sandboxed iframes. Please upgrade to a modern browser.');
+			return '';
+		}
+		iframe['sandbox'] = 'allow-same-origin';
+		iframe.style.display = 'none';
+		document.body.appendChild(iframe); // necessary so the iframe contains a document
+		var iframedoc = iframe.contentDocument || iframe.contentWindow.document;
+		if (iframedoc.body == null) iframedoc.write("<body></body>"); // null in IE
+		iframedoc.body.innerHTML = input;
+
+		function makeSanitizedCopy(node) {
+			if (node.nodeType == Node.TEXT_NODE) {
+				var newNode = node.cloneNode(true);
+			} else if (node.nodeType == Node.ELEMENT_NODE && (tagWhitelist_[node.tagName] || contentTagWhiteList_[node.tagName])) {
+
+				//remove useless empty spans (lots of those when pasting from MS Outlook)
+				if ((node.tagName == "SPAN" || node.tagName == "B" || node.tagName == "I" || node.tagName == "U")
+					&& node.innerHTML.trim() == "") {
+					return document.createDocumentFragment();
+				}
+
+				if (contentTagWhiteList_[node.tagName])
+					newNode = iframedoc.createElement('DIV'); //convert to DIV
+				else
+					newNode = iframedoc.createElement(node.tagName);
+
+				for (var i = 0; i < node.attributes.length; i++) {
+					var attr = node.attributes[i];
+					if (attributeWhitelist_[attr.name]) {
+						if (attr.name == "style") {
+							for (s = 0; s < node.style.length; s++) {
+								var styleName = node.style[s];
+								if (cssWhitelist_[styleName])
+									newNode.style.setProperty(styleName, node.style.getPropertyValue(styleName));
+							}
+						}
+						else {
+							if (uriAttributes_[attr.name]) { //if this is a "uri" attribute, that can have "javascript:" or something
+								if (attr.value.indexOf(":") > -1 && !startsWithAny(attr.value, schemaWhiteList_))
+									continue;
+							}
+							newNode.setAttribute(attr.name, attr.value);
+						}
+					}
+				}
+				for (i = 0; i < node.childNodes.length; i++) {
+					var subCopy = makeSanitizedCopy(node.childNodes[i]);
+					newNode.appendChild(subCopy, false);
+				}
+			} else {
+				newNode = document.createDocumentFragment();
+			}
+			return newNode;
+		};
+
+		var resultElement = makeSanitizedCopy(iframedoc.body);
+		document.body.removeChild(iframe);
+		return resultElement.innerHTML
+			.replace(/<br[^>]*>(\S)/g, "<br>\n$1")
+			.replace(/div><div/g, "div>\n<div"); //replace is just for cleaner code
+	}
+
+	function startsWithAny(str, substrings) {
+		for (var i = 0; i < substrings.length; i++) {
+			if (str.indexOf(substrings[i]) == 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	this.AllowedTags = tagWhitelist_;
+	this.AllowedAttributes = attributeWhitelist_;
+	this.AllowedCssStyles = cssWhitelist_;
+	this.AllowedSchemas = schemaWhiteList_;
+});
 
 /**
  * 表单验证器
