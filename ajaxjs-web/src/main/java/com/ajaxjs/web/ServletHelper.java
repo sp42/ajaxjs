25a79f73141b61a925922255280c91bba6dd556e
frank42a <sp42@qq.com>
2020-03-14 21:27:27 +0800

    no commit message

--- ajaxjs-web/src/main/java/com/ajaxjs/web/ServletHelper.java
+++ ajaxjs-web/src/main/java/com/ajaxjs/web/ServletHelper.java
@@ -16,15 +16,14 @@
 package com.ajaxjs.web;
 
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.regex.Pattern;
 
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServletRequest;
+
+import com.ajaxjs.util.map.MapTool;
 
 /**
  * Servlet 辅助工具类
@@ -33,30 +32,6 @@ import javax.servlet.http.HttpServletRequest;
  *
  */
 public class ServletHelper {
-
-	/**
-	 * 遍历注解的配置，需要什么类，收集起来，放到一个 hash 之中， Servlet 或 Filter 通用
-	 * 
-	 * @param getInitParameterNames
-	 * @param getValue
-	 * @return 配置 Map
-	 */
-	private static Map<String, String> initParams2map(Supplier<Enumeration<String>> getInitParameterNames,
-			Function<String, String> getValue) {
-		Map<String, String> map = new HashMap<>();
-
-		Enumeration<String> initParams = getInitParameterNames.get();
-
-		while (initParams.hasMoreElements()) {
-			String key = initParams.nextElement();
-			String value = getValue.apply(key);
-
-			map.put(key, value);
-		}
-
-		return map;
-	}
-
 	/**
 	 * 将过滤器的配置转换为 Map
 	 * 
@@ -64,7 +39,7 @@ public class ServletHelper {
 	 * @return 过滤器配置的 Map 结构
 	 */
 	public static Map<String, String> initFilterConfig2Map(FilterConfig config) {
-		return initParams2map(() -> config.getInitParameterNames(), key -> config.getInitParameter(key));
+		return MapTool.emu2map(config.getInitParameterNames(), key -> config.getInitParameter(key));
 	}
 
 	/**
@@ -74,7 +49,7 @@ public class ServletHelper {
 	 * @return Servlet 配置的 Map 结构
 	 */
 	public static Map<String, String> initServletConfig2Map(ServletConfig config) {
-		return initParams2map(() -> config.getInitParameterNames(), key -> config.getInitParameter(key));
+		return MapTool.emu2map(config.getInitParameterNames(), key -> config.getInitParameter(key));
 	}
 
 	private static final Pattern p = Pattern
