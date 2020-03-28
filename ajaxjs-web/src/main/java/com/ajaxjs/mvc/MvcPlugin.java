2b9e4137116f5c0a79171bd7799b2a77c2979afe
frank42a <sp42@qq.com>
2020-03-28 23:03:37 +0800

    no commit message

--- ajaxjs-web/src/main/java/com/ajaxjs/mvc/MvcPlugin.java
+++ ajaxjs-web/src/main/java/com/ajaxjs/mvc/MvcPlugin.java
@@ -1,11 +1,23 @@
 package com.ajaxjs.mvc;
 
+import java.util.function.Consumer;
+
+import javax.servlet.ServletContext;
+
 /**
  * MVC 框架的插件
  * 
  * @author sp42 frank@ajaxjs.com
  *
  */
-public interface MvcPlugin {
+public abstract class MvcPlugin {
+	private Consumer<ServletContext> onServletStartUp;
 
+	public Consumer<ServletContext> getOnServletStartUp() {
+		return onServletStartUp;
+	}
+
+	public void setOnServletStartUp(Consumer<ServletContext> onServletStartUp) {
+		this.onServletStartUp = onServletStartUp;
+	}
 }
