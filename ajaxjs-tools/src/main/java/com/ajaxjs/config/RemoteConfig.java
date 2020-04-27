88d96a97ee002334e4b9c0e6753baf72d4b72f83
frank42a <sp42@qq.com>
2020-04-27 23:32:19 +0800

    Remove ajaxjs-tools

--- ajaxjs-tools/src/main/java/com/ajaxjs/config/RemoteConfig.java
+++
@@ -1,8 +0,0 @@
-package com.ajaxjs.config;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RemoteConfig extends Remote {
-	public String getStru() throws RemoteException;
-}
--- ajaxjs-tools/src/main/java/com/ajaxjs/framework/service/ValidObj.java
+++
@@ -1,9 +0,0 @@
-package com.ajaxjs.framework.service;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface ValidObj {
-
-}
