b9cd6555b38bbad95f60468861e7ba693655a05e
frank42a <sp42@qq.com>
2020-05-24 00:41:18 +0800

    no commit message

---
+++ ajaxjs-third/src/main/java/com/ajaxjs/payment/wxpay/BaseResponse.java
@@ -0,0 +1,85 @@
+package com.ajaxjs.payment.wxpay;
+
+/**
+ * 响应内容基类
+ * 
+ * @author sp42 frank@ajaxjs.com
+ *
+ */
+public class BaseResponse {
+
+	/**
+	 * 返回状态码
+	 */
+	private String return_code;
+
+	/**
+	 * 返回信息
+	 */
+	private String return_msg;
+
+	/**
+	 * 结果代码
+	 */
+	private String result_code;
+
+	/**
+	 * 错误代码
+	 */
+	private String error_code;
+
+	/**
+	 * 错误描述
+	 */
+	private String error_code_desc;
+
+	public String getReturn_code() {
+		return return_code;
+	}
+
+	public void setReturn_code(String return_code) {
+		this.return_code = return_code;
+	}
+
+	public String getReturn_msg() {
+		return return_msg;
+	}
+
+	public void setReturn_msg(String return_msg) {
+		this.return_msg = return_msg;
+	}
+
+	public String getResult_code() {
+		return result_code;
+	}
+
+	public void setResult_code(String result_code) {
+		this.result_code = result_code;
+	}
+
+	public String getError_code() {
+		return error_code;
+	}
+
+	public void setError_code(String error_code) {
+		this.error_code = error_code;
+	}
+
+	public String getError_code_desc() {
+		return error_code_desc;
+	}
+
+	public void setError_code_desc(String error_code_desc) {
+		this.error_code_desc = error_code_desc;
+	}
+
+	/**
+	 * 统一下单响应是否成功
+	 * 
+	 * @return true 表示成功
+	 */
+	public boolean isSuccess() {
+		return "SUCCESS".equals(return_code) && "SUCCESS".equals(result_code);
+	}
+
+}
---
+++ ajaxjs-third/src/main/java/com/ajaxjs/payment/wxpay/PerpayReturn.java
@@ -0,0 +1,73 @@
+package com.ajaxjs.payment.wxpay;
+
+/**
+ * 统一下单后返回的结果
+ * 
+ * @author sp42 frank@ajaxjs.com
+ *
+ */
+public class PerpayReturn extends BaseResponse {
+	private String appid;
+	private String mch_id;
+	private String nonce_str;
+	private String sign;
+	private String prepay_id;
+	private String trade_type;
+	private String code_url;
+
+	public String getAppid() {
+		return appid;
+	}
+
+	public void setAppid(String appid) {
+		this.appid = appid;
+	}
+
+	public String getMch_id() {
+		return mch_id;
+	}
+
+	public void setMch_id(String mch_id) {
+		this.mch_id = mch_id;
+	}
+
+	public String getNonce_str() {
+		return nonce_str;
+	}
+
+	public void setNonce_str(String nonce_str) {
+		this.nonce_str = nonce_str;
+	}
+
+	public String getSign() {
+		return sign;
+	}
+
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+
+	public String getPrepay_id() {
+		return prepay_id;
+	}
+
+	public void setPrepay_id(String prepay_id) {
+		this.prepay_id = prepay_id;
+	}
+
+	public String getTrade_type() {
+		return trade_type;
+	}
+
+	public void setTrade_type(String trade_type) {
+		this.trade_type = trade_type;
+	}
+
+	public String getCode_url() {
+		return code_url;
+	}
+
+	public void setCode_url(String code_url) {
+		this.code_url = code_url;
+	}
+}
