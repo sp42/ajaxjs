923f1615bbfb149703f15b369b7b9b74cf8ea54c
frank42a <sp42@qq.com>
2020-07-30 17:10:32 +0800

    no commit message

---
+++ ajaxjs-demo/src/main/java/com/demo/mvc/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.demo.mvc;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import com.ajaxjs.web.mvc.controller.IController;
+
+@Path("/SayHello") // 定义URL路径
+public class HelloWorldController implements IController {
+	@GET	// 绑定 HTTP GET 方法，用户一访问 GET /SayHello，就会跑到这方法来
+	public String helloWorld() {
+		return "html::Hello World!"; // 返回字符串就是要显示的内容，html:: 是固有的前缀，不会显示
+	}
+}
---
+++ ajaxjs-demo/src/main/java/com/demo/mvc/MyController.java
@@ -0,0 +1,40 @@
+package com.demo.mvc;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+
+import com.ajaxjs.web.mvc.controller.IController;
+
+@Path("/foo") // 定义总的URL路径
+public class MyController implements IController {
+	@GET
+	public String showHTML() {
+		return "html::Hello World!";
+	}
+
+	@GET
+	@Path("jsp") // 与类的@Path注解里的路径组合
+	public String gotoJSP() {
+		return "/jsp/my-jsp.jsp";
+	}
+
+	@POST
+	@Path("/bar/blog") // 注意斜杠/开头，表示不与类的@Path注解里的路径组合
+	public String redirect() {
+		return "redirect::http://blog.csdn.net/zhangxin09";
+	}
+
+	@PUT
+	public String showJSON() {
+		return "json::{\"name\":\"Jack\"}";
+	}
+
+	@DELETE
+	@Path("/admin/{root}/{id}")
+	public String editUI() {
+		return "app/article-edit";
+	}
+}
