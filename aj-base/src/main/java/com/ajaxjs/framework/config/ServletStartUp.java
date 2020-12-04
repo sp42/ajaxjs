24cac35469091e654c2687bf1ff4794ec987d02c
frank42a <sp42@qq.com>
2020-12-05 01:33:53 +0800

    no commit message

---
+++ aj-base/src/main/java/com/ajaxjs/framework/config/ServletStartUp.java
@@ -0,0 +1,13 @@
+package com.ajaxjs.framework.config;
+
+import javax.servlet.ServletContext;
+
+/**
+ * 当 Servlet 启动时执行的回调
+ * 
+ * @author sp42 frank@ajaxjs.com
+ *
+ */
+public interface ServletStartUp {
+	public void onStartUp(ServletContext cxt);
+}
