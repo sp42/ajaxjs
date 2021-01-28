97971b0f1b84940eff4558ecfc65ee72d6ce18cd
frank42a <sp42@qq.com>
2021-01-28 14:13:34 +0800

    no commit message

--- aj-base/src/main/java/com/ajaxjs/sql/SnowflakeIdWorker.java
+++ aj-base/src/main/java/com/ajaxjs/sql/SnowflakeIdWorker.java
@@ -50,7 +50,6 @@ public class SnowflakeIdWorker {
 	/** 上次生成ID的时间截 */
 	private long lastTimestamp = -1L;
 
-	// ==============================Constructors=====================================
 	/**
 	 * 构造函数
 	 * 
@@ -58,46 +57,37 @@ public class SnowflakeIdWorker {
 	 * @param datacenterId 数据中心ID (0~31)
 	 */
 	public SnowflakeIdWorker(long workerId, long datacenterId) {
-		if (workerId > maxWorkerId || workerId < 0) {
+		if (workerId > maxWorkerId || workerId < 0)
 			throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0", maxWorkerId));
-		}
-		if (datacenterId > maxDatacenterId || datacenterId < 0) {
+
+		if (datacenterId > maxDatacenterId || datacenterId < 0)
 			throw new IllegalArgumentException(String.format("datacenter Id can't be greater than %d or less than 0", maxDatacenterId));
-		}
+
 		this.workerId = workerId;
 		this.datacenterId = datacenterId;
 	}
 
-	// ==============================Methods==========================================
 	/**
 	 * 获得下一个ID (该方法是线程安全的)
 	 * 
 	 * @return SnowflakeId
 	 */
 	public synchronized long nextId() {
-		long timestamp = timeGen();
+		long timestamp = System.currentTimeMillis();
 
 		// 如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过这个时候应当抛出异常
-		if (timestamp < lastTimestamp) {
+		if (timestamp < lastTimestamp)
 			throw new RuntimeException(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
-		}
 
-		// 如果是同一时间生成的，则进行毫秒内序列
-		if (lastTimestamp == timestamp) {
+		if (lastTimestamp == timestamp) {// 如果是同一时间生成的，则进行毫秒内序列
 			sequence = (sequence + 1) & sequenceMask;
-			// 毫秒内序列溢出
-			if (sequence == 0) {
-				// 阻塞到下一个毫秒,获得新的时间戳
-				timestamp = tilNextMillis(lastTimestamp);
-			}
-		}
-		// 时间戳改变，毫秒内序列重置
-		else {
-			sequence = 0L;
-		}
 
-		// 上次生成ID的时间截
-		lastTimestamp = timestamp;
+			if (sequence == 0)// 毫秒内序列溢出
+				timestamp = tilNextMillis(lastTimestamp);// 阻塞到下一个毫秒,获得新的时间戳
+		} else
+			sequence = 0L;// 时间戳改变，毫秒内序列重置
+
+		lastTimestamp = timestamp;// 上次生成ID的时间截
 
 		// 移位并通过或运算拼到一起组成64位的ID
 		return ((timestamp - twepoch) << timestampLeftShift) //
@@ -113,20 +103,12 @@ public class SnowflakeIdWorker {
 	 * @return 当前时间戳
 	 */
 	protected long tilNextMillis(long lastTimestamp) {
-		long timestamp = timeGen();
-		while (timestamp <= lastTimestamp) {
-			timestamp = timeGen();
-		}
-		return timestamp;
-	}
+		long timestamp = System.currentTimeMillis();
 
-	/**
-	 * 返回以毫秒为单位的当前时间
-	 * 
-	 * @return 当前时间(毫秒)
-	 */
-	protected long timeGen() {
-		return System.currentTimeMillis();
+		while (timestamp <= lastTimestamp)
+			timestamp = System.currentTimeMillis();
+
+		return timestamp;
 	}
 
 	public static final SnowflakeIdWorker idWorker = new SnowflakeIdWorker(0, 0);
