8698ac599ed3f425633a3146db04a43149f09627
frank42a <sp42@qq.com>
2021-03-07 17:57:33 +0800

    no commit message

--- aj-base/src/main/java/com/ajaxjs/cms/section/SectionController.java
+++ aj-base/src/main/java/com/ajaxjs/cms/section/SectionController.java
@@ -1,6 +1,5 @@
 package com.ajaxjs.cms.section;
 
-import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.DELETE;
@@ -10,10 +9,8 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import com.ajaxjs.cms.common.Catalog;
 import com.ajaxjs.framework.BaseController;
 import com.ajaxjs.framework.config.ConfigService;
 import com.ajaxjs.framework.filter.DataBaseFilter;
