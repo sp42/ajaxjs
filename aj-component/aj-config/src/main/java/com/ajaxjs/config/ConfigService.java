0647d32bb2cb0e14309d159a0badf6b562471f86
frank42a <sp42@qq.com>
2021-04-30 13:34:35 +0800

    no commit message

---
+++ aj-component/aj-config/src/main/java/com/ajaxjs/config/ConfigService.java
@@ -0,0 +1,49 @@
+package com.ajaxjs.config;
+
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+
+/**
+ * 配置业务
+ * 
+ * @author sp42 frank@ajaxjs.com
+ *
+ */
+public interface ConfigService {
+	/**
+	 * 加载 JSON 配置
+	 * 
+	 * @param cfgPath 配置文件所在路径
+	 */
+	public void load(String cfgPath);
+
+	/**
+	 * 后台编辑 JSON 时候所需要的数据：一个是配置本身，另外是配置说明（Scheme）
+	 */
+	public String[] loadEdit();
+
+	/**
+	 * 保存配置
+	 * 
+	 * @param map 前端提交过来的配置数据
+	 * @param ctx Web 上下文，配置保存在这里
+	 */
+	public void saveAllconfig(Map<String, Object> map, ServletContext ctx);
+
+	/**
+	 * 所有的配置保存在这个 config 中
+	 */
+	public Map<String, Object> getConfig();
+
+	/**
+	 * 所有的配置保存在这个 config 中（扁平化处理过的）
+	 */
+	public Map<String, Object> getFlatConfig();
+
+	/**
+	 * 是否加载成功
+	 */
+	public boolean isLoaded();
+
+}
