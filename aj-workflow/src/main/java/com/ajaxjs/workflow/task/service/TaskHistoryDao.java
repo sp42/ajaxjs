da175ea1bbc07e6be67fd43d5af288d1b78d2be2
frank42a <sp42@qq.com>
2022-04-23 01:00:39 +0000

    删除文件 aj-workflow

--- aj-workflow/src/main/java/com/ajaxjs/workflow/task/service/TaskHistoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.ajaxjs.workflow.task.service;
-
-import com.ajaxjs.sql.annotation.TableName;
-import com.ajaxjs.sql.orm.IBaseDao;
-import com.ajaxjs.workflow.task.TaskHistory;
-
-@TableName(value = "wf_task_history", beanClass = TaskHistory.class)
-public interface TaskHistoryDao extends IBaseDao<TaskHistory> {
-}
--- aj-workflow/src/main/java/com/ajaxjs/workflow/util/surrogate/SurrogateDao.java
+++
@@ -1,8 +0,0 @@
-package com.ajaxjs.workflow.util.surrogate;
-
-import com.ajaxjs.sql.annotation.TableName;
-import com.ajaxjs.sql.orm.IBaseDao;
-
-@TableName(value = "wf_surrogate", beanClass = Surrogate.class)
-public interface SurrogateDao extends IBaseDao<Surrogate> {
-}
