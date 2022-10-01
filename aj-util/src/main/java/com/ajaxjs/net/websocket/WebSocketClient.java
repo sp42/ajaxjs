a518ef9f82d39c7a92347ee50f7423a3f07dd17e
frank42a <sp42@qq.com>
2022-10-01 20:18:34 +0800

    no commit message

--- aj-util/src/main/java/com/ajaxjs/net/websocket/WebSocketClient.java
+++ aj-util/src/main/java/com/ajaxjs/net/websocket/WebSocketClient.java
@@ -3,6 +3,10 @@ package com.ajaxjs.net.websocket;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiConsumer;
 
 import javax.websocket.ClientEndpoint;
@@ -15,6 +19,7 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.WebSocketContainer;
 
+import com.ajaxjs.util.ThreadUtil;
 import com.ajaxjs.util.logger.LogHelper;
 
 /**
@@ -31,6 +36,8 @@ public class WebSocketClient {
 
 	protected Session userSession;
 
+	private String server;
+
 	/**
 	 * 创建 WebSocket 客户端
 	 */
@@ -44,6 +51,14 @@ public class WebSocketClient {
 	 * @param server 服务器地址
 	 */
 	public void connect(String server) {
+		this.server = server;
+		connect();
+	}
+
+	/**
+	 * 连接 WebSocket 服务器
+	 */
+	public void connect() {
 		try {
 			userSession = container.connectToServer(this, new URI(server));
 		} catch (DeploymentException | URISyntaxException | IOException e) {
@@ -61,6 +76,7 @@ public class WebSocketClient {
 	public void sendMessage(String msg) {
 		try {
 			userSession.getBasicRemote().sendText(msg);
+
 		} catch (IOException e) {
 			LOGGER.warning(e);
 		}
@@ -69,10 +85,16 @@ public class WebSocketClient {
 	@OnOpen
 	public void onOpen(Session session) {
 		LOGGER.info("WebSocket Connected");
+		tryReconnect.set(false);
+		circlePing();
 	}
 
 	@OnClose
 	public void onClose(Session session, CloseReason closeReason) {
+		if (end.get())
+			return;
+
+		needReconnect();
 	}
 
 	private BiConsumer<Session, String> onMessage;
@@ -92,9 +114,88 @@ public class WebSocketClient {
 	}
 
 	/**
+	 * 需要ping标识
+	 */
+	private AtomicBoolean needPing = new AtomicBoolean(true);
+
+	/**
+	 * 尝试重连标识
+	 */
+	private AtomicBoolean tryReconnect = new AtomicBoolean(false);
+
+	/**
+	 * 重连次数
+	 */
+	private AtomicInteger reConnectTimes = new AtomicInteger(0);
+
+	/**
+	 * 连接结束标识
+	 */
+	private AtomicBoolean end = new AtomicBoolean(false);
+
+	private static ByteBuffer PING_PAYLOAD = null;
+
+	public void circlePing() {
+		if (PING_PAYLOAD == null)
+			PING_PAYLOAD = ByteBuffer.wrap("Ping".getBytes());
+
+		new Thread(() -> {
+			while (needPing.get()) {
+				if (userSession != null && userSession.isOpen())
+					try {
+						userSession.getBasicRemote().sendPing(PING_PAYLOAD);
+					} catch (IllegalArgumentException | IOException e) {
+						LOGGER.warning(e);
+					}
+
+				ThreadUtil.sleep(5, TimeUnit.SECONDS);
+			}
+
+			LOGGER.warning("[]Ping循环关闭");
+		}).start();
+	}
+
+	/**
+	 * 重新连接
+	 */
+	private void needReconnect() {
+		ThreadUtil.sleep(3, TimeUnit.SECONDS);
+		int cul = reConnectTimes.incrementAndGet();
+
+		if (cul > 3) {
+			disconnect();// close("real stop");
+			throw new NullPointerException("服务端断连，3次重连均失败");
+		}
+
+		LOGGER.warning("[{0}]第[{1}]次断开重连", cul);
+
+		if (tryReconnect.get()) {
+			LOGGER.warning("[{}]第[{}]次断开重连结果 -> 连接正在重连，本次重连请求放弃", cul);
+			needReconnect();
+
+			return;
+		}
+
+		try {
+			tryReconnect.set(true);
+
+			if (userSession != null && userSession.isOpen()) {
+				LOGGER.warning("[{}]第[{}]次断开重连，关闭旧连接", cul);
+				disconnect();
+			}
+
+			container = ContainerProvider.getWebSocketContainer();
+			connect();
+		} catch (Exception exception) {
+			LOGGER.warning("[{}]第[{}]次断开重连结果 -> 连接正在重连，重连异常:[{}]", cul, exception.getMessage());
+			needReconnect();
+		} finally {
+			tryReconnect.set(false);
+		}
+	}
+
+	/**
 	 * 关闭链接
-	 * 
-	 * @throws IOException
 	 */
 	public void disconnect() {
 		try {
