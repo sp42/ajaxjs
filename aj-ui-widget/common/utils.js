227007ca035003c94c32234f33d48afa92dcd73e
Frank <sp42@qq.com>
2022-12-13 18:54:33 +0800

    ci

--- aj-ui-widget/common/utils.js
+++ aj-ui-widget/common/utils.js
@@ -201,3 +201,15 @@ function openDownloadDialog(url, saveName) {
 
     aLink.dispatchEvent(event);
 }
+
+function getQueryParam(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+
+    return (false);
+}
