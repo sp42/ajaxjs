5e6d675bb0c3194ac190b94496e2e682dc7ea7fb
Frank <sp42@qq.com>
2023-03-15 19:36:36 +0800

    ci

--- aj-framework/src/main/java/com/ajaxjs/spring/little/BaseLittleWebAppInit.java
+++ aj-framework/src/main/java/com/ajaxjs/spring/little/BaseLittleWebAppInit.java
@@ -11,24 +11,35 @@ import com.ajaxjs.spring.BaseSpringWebInitializer;
 import com.ajaxjs.util.logger.LogHelper;
 
 public abstract class BaseLittleWebAppInit extends BaseSpringWebInitializer {
-	private static final LogHelper LOGGER = LogHelper.getLog(BaseLittleWebAppInit.class);
+    private static final LogHelper LOGGER = LogHelper.getLog(BaseLittleWebAppInit.class);
 
-	public abstract Class<?> getConfigClz();
+    public abstract Class<?> getConfigClz();
 
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		// 通过注解的方式初始化Spring的上下文
-		AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
-		// 注册spring的配置类（替代传统项目中xml的configuration）
-		ac.register(getConfigClz());
-		ac.refresh();
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        // 通过注解的方式初始化 Spring 的上下文
+        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
+        // 注册 Spring 的配置类（替代传统项目中 xml 的 configuration）
+        if (getConfigClz() != null)
+            ac.register(getConfigClz());
+        ac.refresh();
 
-		// 绑定 servlet
-		ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcherServlet", new DispatcherServlet(ac));
-		registration.setLoadOnStartup(1);// 设置 tomcat 启动立即加载 servlet
-		registration.addMapping("/*"); // 浏览器访问 uri
+        // 绑定 servlet
+        ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcherServlet", new DispatcherServlet(ac));
+        registration.setLoadOnStartup(1);// 设置 tomcat 启动立即加载 servlet
+        registration.addMapping("/"); // 浏览器访问 uri
 
-		initUpload(servletContext, registration);
-		LOGGER.info("WEB 程序启动完毕");
-	}
+        // 设置 tomcat 启动后的工作目录
+//        Context context1 = tomcat.addContext("/", new File(System.getProperty("java.io.tmpdir")).getAbsolutePath());
+//        Tomcat.addServlet(context1, "my", new DispatcherServlet(springCtx)).setLoadOnStartup(1);
+//        context1.addServletMappingDecoded("/", "my");
+
+        // 初始化 ContextConfig 配置
+//        StandardContext ctx;
+//        ctx.addLifecycleListener(new ContextConfig());
+//        ctx.addApplicationEventListener(new ContextLoaderListener(ac));
+
+        initUpload(servletContext, registration);
+        LOGGER.info("WEB 程序启动完毕");
+    }
 }
--- aj-framework/src/main/java/com/ajaxjs/spring/little/LittleSpringBoot.java
+++ aj-framework/src/main/java/com/ajaxjs/spring/little/LittleSpringBoot.java
@@ -1,7 +1,5 @@
 package com.ajaxjs.spring.little;
 
-import java.io.File;
-
 import org.apache.catalina.Engine;
 import org.apache.catalina.Host;
 import org.apache.catalina.LifecycleException;
@@ -13,119 +11,63 @@ import org.apache.catalina.core.StandardHost;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
+import java.io.File;
+
+/**
+ * 嵌入式使用 Tomcat
+ */
 public class LittleSpringBoot {
-	public static void init2() {}
-	/**
-	 * 启动程序
-	 * 
-	 * @param port
-	 */
-	public static void init(int port) {
-		Tomcat tomcat = new Tomcat();
-		tomcat.getHost().setAutoDeploy(false);
-		tomcat.enableNaming();
+    /**
+     * 启动程序
+     *
+     * @param port
+     */
+    public static void init(int port) {
+        Tomcat tomcat = new Tomcat();
+        tomcat.enableNaming();
+        tomcat.getHost().setAutoDeploy(false);
+        tomcat.getHost().setAppBase("webapp");// 在对应的 host 下面创建一个 context 并制定他的工作路径,会加载该目录下的所有 class 文件,或者静态文件
+//        tomcat.setBaseDir(Thread.currentThread().getContextClassLoader().getResource("").getPath()); // 设置 tomcat 启动后的工作目录
+//        System.out.println(Thread.currentThread().getContextClassLoader().getResource("").getPath());
 
-		setTomcatContext(tomcat);
-		System.out.println(new File(System.getProperty("java.io.tmpdir")).getAbsolutePath());
+        // 读取项目路径
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File("C:\\code\\auth-git\\security-console-sync\\security-console-sync\\src\\main\\webapp").getAbsolutePath());
+        ctx.setReloadable(false);// 禁止重新载入
+        WebResourceRoot resources = new StandardRoot(ctx);// 创建WebRoot
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", new File("target/classes").getAbsolutePath(), "/"));// tomcat 内部读取 Class 执行
 
-//        WebApplicationContext springCtx = getSpringWebAppContext();
-//        // 设置 tomcat 启动后的工作目录
-//        Context context1 = tomcat.addContext("/", new File(System.getProperty("java.io.tmpdir")).getAbsolutePath());
-//        Tomcat.addServlet(context1, "my", new DispatcherServlet(springCtx)).setLoadOnStartup(1);
-//        context1.addServletMappingDecoded("/", "my");
+        // 创建连接器，并且添加对应的连接器，同时连接器指定端口 设置 IO 协议
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(port);
+        connector.setThrowOnFailure(true);
 
-		setConnector(tomcat, port);
+        tomcat.getService().addConnector(connector);// 只能设置一个 service,直接拿默认的
+        tomcat.setConnector(connector); // 设置执行器
 
-		try {
-			tomcat.start(); // tomcat 启动
-		} catch (LifecycleException e) {
-			throw new RuntimeException(e);
-		}
+        try {
+            tomcat.start(); // tomcat 启动
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+        }
 
-		tomcat.getServer().await(); // 保持主线程不退出，让其阻塞，不让当前线程结束，等待处理请求
+        tomcat.getServer().await(); // 保持主线程不退出，让其阻塞，不让当前线程结束，等待处理请求
+    }
 
-//        setBaseDir(tomcat);
-	}
+    /**
+     * 一般不用设置，使用默认即可
+     *
+     * @param tomcat
+     */
+    private static void setEngineHost(Tomcat tomcat) {
+        Engine engine = new StandardEngine();// 创建一个引擎,放入 service 中
+        engine.setDefaultHost("localhost");
+        engine.setName("myTomcat");
 
-	/**
-	 * 在对应的 host 下面创建一个 context 并制定他的工作路径,会加载该目录下的所有 class 文件,或者静态文件
-	 *
-	 * @param tomcat
-	 */
-	private static void setTomcatContext(Tomcat tomcat) {
-		tomcat.getHost().setAppBase("webapp");
+        Host host = new StandardHost(); // 添加 host
+        host.setName("localhost");
 
-		// 读取项目路径
-		StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File("src/main/webapp").getAbsolutePath());
-		ctx.setReloadable(false);// 禁止重新载入
-
-		// tomcat 内部读取 Class 执行
-		File additionWebInfClasses = new File("target/classes");// class 文件读取地址
-		WebResourceRoot resources = new StandardRoot(ctx);// 创建WebRoot
-		resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
-
-//        WebApplicationContext springCtx = getSpringWebAppContext();
-		// 初始化 ContextConfig 配置
-//        ctx.addLifecycleListener(new ContextConfig());
-//        ctx.addApplicationEventListener(new ContextLoaderListener(springCtx));
-	}
-
-	/**
-	 * 创建连接器,并且添加对应的连接器,同时连接器指定端口 设置 io 协议
-	 *
-	 * @param tomcat
-	 * @param port
-	 */
-	private static void setConnector(Tomcat tomcat, int port) {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setPort(port);
-		connector.setThrowOnFailure(true);
-
-		tomcat.getService().addConnector(connector);// 只能设置一个 service,直接拿默认的
-		tomcat.setConnector(connector); // 设置执行器
-	}
-
-	/**
-	 * 通过注解的方式初始化 Spring 的上下文
-	 *
-	 * @return
-	 */
-	private static WebApplicationContext getSpringWebAppContext() {
-		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-//        context.register(AutoConfig.class);
-		context.refresh();
-
-		return context;
-	}
-
-	/**
-	 * 一般不用设置，使用默认即可
-	 *
-	 * @param tomcat
-	 */
-	private static void setEngineHost(Tomcat tomcat) {
-		Engine engine = new StandardEngine();// 创建一个引擎,放入 service 中
-		engine.setDefaultHost("localhost");
-		engine.setName("myTomcat");
-
-		Host host = new StandardHost(); // 添加 host
-		host.setName("localhost");
-
-		engine.addChild(host);
-		tomcat.getService().setContainer(engine);
-	}
-
-	/**
-	 * 设置 tomcat 启动后的工作目录
-	 *
-	 * @param tomcat
-	 */
-	private static void setBaseDir(Tomcat tomcat) {
-		String baseDir = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-		tomcat.setBaseDir(baseDir);
-	}
-
+        engine.addChild(host);
+        tomcat.getService().setContainer(engine);
+    }
 }
