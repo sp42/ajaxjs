d80480167d92dc58657dd2ef84e58748801e36d6
Frank <sp42@qq.com>
2023-08-26 01:20:09 +0800

    ci

--- aj-backend/aj-framework/aj-framework/src/main/java/com/ajaxjs/framework/BusinessException.java
+++ aj-backend/aj-framework/aj-framework/src/main/java/com/ajaxjs/framework/BusinessException.java
@@ -1,10 +1,17 @@
 package com.ajaxjs.framework;
 
+/**
+ * 业务异常
+ */
 public class BusinessException extends RuntimeException {
     public BusinessException() {
-
     }
 
+    /**
+     * 业务异常
+     *
+     * @param msg 业务异常
+     */
     public BusinessException(String msg) {
         super(msg);
     }
