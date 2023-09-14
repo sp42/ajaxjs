6725ef4cb7654c36e4edaa3676010b782593bfac
Frank <sp42@qq.com>
2023-09-14 21:44:50 +0800

    ci

--- aj-backend/aj-framework/aj-framework/src/main/java/com/ajaxjs/data/CRUD.java
+++ aj-backend/aj-framework/aj-framework/src/main/java/com/ajaxjs/data/CRUD.java
@@ -39,6 +39,15 @@ public abstract class CRUD {
         return writer;
     }
 
+    /**
+     * 查询单行单列的记录
+     *
+     * @param clz    返回的类型
+     * @param sql    执行的 SQL
+     * @param params SQL 参数列表（选填项，能对应 SQL 里面的`?`的插值符）
+     * @param <T>    返回的类型
+     * @return 单行单列记录
+     */
     public static <T> T queryOne(Class<T> clz, String sql, Object... params) {
         return jdbcReaderFactory().queryOne(sql, clz, params);
     }
@@ -47,25 +56,52 @@ public abstract class CRUD {
      * 查询单笔记录，以 Map 格式返回
      *
      * @param sql    SQL 语句
-     * @param params 参数列表，可以为 null 不传
+     * @param params SQL 参数列表（选填项，能对应 SQL 里面的`?`的插值符）
      * @return 查询结果，如果为 null 表示没数据
      */
     public static Map<String, Object> info(String sql, Object... params) {
         return jdbcReaderFactory().queryAsMap(sql, params);
     }
 
+    /**
+     * 查询单笔记录，以 Map 格式返回
+     *
+     * @param sqlId     SQL Id，于 XML 里的索引
+     * @param paramsMap Map 格式的参数（若没有可传 null）
+     * @param params    SQL 参数列表（选填项，能对应 SQL 里面的`?`的插值符）
+     * @return 查询结果，如果为 null 表示没数据
+     */
     public static Map<String, Object> infoMap(String sqlId, Map<String, Object> paramsMap, Object... params) {
         String sql = SmallMyBatis.handleSql(paramsMap, sqlId);
 
         return info(sql, params);
     }
 
+    /**
+     * 查询单笔记录，以 Java Bean 格式返回
+     *
+     * @param sqlId     SQL Id，于 XML 里的索引
+     * @param beanClz   返回的 Bean 类型
+     * @param paramsMap Map 格式的参数（若没有可传 null）
+     * @param params    SQL 参数列表（选填项，能对应 SQL 里面的`?`的插值符）
+     * @param <T>       返回的 Bean 类型
+     * @return 查询单笔记录，以 Java Bea 格式返回
+     */
     public static <T> T info(String sqlId, Class<T> beanClz, Map<String, Object> paramsMap, Object... params) {
         String sql = SmallMyBatis.handleSql(paramsMap, sqlId);
 
         return jdbcReaderFactory().queryAsBean(beanClz, sql, params);
     }
 
+    /**
+     * 查询单笔记录，以 Java Bean 格式返回
+     *
+     * @param beanClz 返回的 Bean 类型
+     * @param sql     SQL 语句
+     * @param params  SQL 参数列表（选填项，能对应 SQL 里面的`?`的插值符）
+     * @param <T>     返回的 Bean 类型
+     * @return 查询单笔记录，以 Java Bea 格式返回
+     */
     public static <T> T info(Class<T> beanClz, String sql, Object... params) {
         return jdbcReaderFactory().queryAsBean(beanClz, sql, params);
     }
@@ -74,7 +110,7 @@ public abstract class CRUD {
      * 查询列表记录，以 List Map 格式返回
      *
      * @param sql    SQL 语句
-     * @param params 参数列表，可以为 null 不传
+     * @param params SQL 参数列表（选填项，能对应 SQL 里面的`?`的插值符）
      * @return 查询结果，如果没数据返回一个空 List
      */
     public static List<Map<String, Object>> list(String sql, Object... params) {
@@ -86,7 +122,10 @@ public abstract class CRUD {
     /**
      * 查询列表记录，以 List Map 格式返回
      *
-     * @param sqlId SQL 语句
+     * @param sqlId     SQL Id，于 XML 里的索引
+     * @param paramsMap Map 格式的参数（若没有可传 null）
+     * @param params    SQL 参数列表（选填项，能对应 SQL 里面的`?`的插值符）
+     * @return 查询结果，如果没数据返回一个空 List
      */
     public static List<Map<String, Object>> listMap(String sqlId, Map<String, Object> paramsMap, Object... params) {
         String sql = SmallMyBatis.handleSql(paramsMap, sqlId);
@@ -94,24 +133,12 @@ public abstract class CRUD {
         return list(sql, params);
     }
 
-    public static <T> PageResult<T> page(Class<T> beanClz, String sql, Map<String, Object> paramsMap) {
-        sql = SmallMyBatis.handleSql(sql, paramsMap);
-
-        return PageEnhancer.page(sql, beanClz);
-    }
-
-    public static <T> PageResult<T> page(String sqlId, Class<T> beanClz, Map<String, Object> paramsMap) {
-        String sql = SmallMyBatis.handleSql(paramsMap, sqlId);
-
-        return PageEnhancer.page(sql, beanClz);
-    }
-
     /**
-     * 查询列表记录，以 List Map 格式返回
+     * 查询列表记录，以 List Java Bean 格式返回
      *
      * @param beanClz 实体 Bean 类型
      * @param sql     SQL 语句
-     * @param params  参数列表，可以为 null 不传
+     * @param params  SQL 参数列表（选填项，能对应 SQL 里面的`?`的插值符）
      * @return 查询结果，如果没数据返回一个空 List
      */
     public static <T> List<T> list(Class<T> beanClz, String sql, Object... params) {
@@ -120,16 +147,64 @@ public abstract class CRUD {
         return ListUtils.getList(list);
     }
 
+    /**
+     * 查询列表记录，以 List Java Bean 格式返回
+     *
+     * @param sqlId     SQL Id，于 XML 里的索引
+     * @param beanClz   实体 Bean 类型
+     * @param paramsMap Map 格式的参数（若没有可传 null）
+     * @param params    SQL 参数列表（选填项，能对应 SQL 里面的`?`的插值符）
+     * @param <T>       实体 Bean 类型
+     * @return 查询结果，如果没数据返回一个空 List
+     */
     public static <T> List<T> list(String sqlId, Class<T> beanClz, Map<String, Object> paramsMap, Object... params) {
+        return listBeanInXml(sqlId, beanClz, paramsMap, params);
+    }
+
+    /**
+     * 查询列表记录，以 List Java Bean 格式返回
+     *
+     * @param sqlId     SQL Id，于 XML 里的索引
+     * @param beanClz   实体 Bean 类型
+     * @param paramsMap Map 格式的参数（若没有可传 null）
+     * @param params    SQL 参数列表（选填项，能对应 SQL 里面的`?`的插值符）
+     * @param <T>       实体 Bean 类型
+     * @return 查询结果，如果没数据返回一个空 List
+     */
+    public static <T> List<T> listBeanInXml(String sqlId, Class<T> beanClz, Map<String, Object> paramsMap, Object... params) {
         String sql = SmallMyBatis.handleSql(paramsMap, sqlId);
 
         return list(beanClz, sql, params);
     }
 
-    public static <T> List<T> listBeanInXml(String sqlId, Class<T> beanClz, Map<String, Object> paramsMap, Object... params) {
+    /**
+     * 分页查询列表记录，以 List Java Bean 格式返回
+     *
+     * @param beanClz   实体 Bean 类型
+     * @param sql       SQL 语句
+     * @param paramsMap Map 格式的参数（若没有可传 null）
+     * @param <T>       实体 Bean 类型
+     * @return 查询结果，如果没数据返回一个空 List
+     */
+    public static <T> PageResult<T> page(Class<T> beanClz, String sql, Map<String, Object> paramsMap) {
+        sql = SmallMyBatis.handleSql(sql, paramsMap);
+
+        return PageEnhancer.page(sql, beanClz);
+    }
+
+    /**
+     * 分页查询列表记录，以 List Java Bean 格式返回
+     *
+     * @param sqlId     SQL Id，于 XML 里的索引
+     * @param beanClz   实体 Bean 类型
+     * @param paramsMap Map 格式的参数（若没有可传 null）
+     * @param <T>       实体 Bean 类型
+     * @return 查询结果，如果没数据返回一个空 List
+     */
+    public static <T> PageResult<T> page(String sqlId, Class<T> beanClz, Map<String, Object> paramsMap) {
         String sql = SmallMyBatis.handleSql(paramsMap, sqlId);
 
-        return list(beanClz, sql, params);
+        return PageEnhancer.page(sql, beanClz);
     }
 
     /**
