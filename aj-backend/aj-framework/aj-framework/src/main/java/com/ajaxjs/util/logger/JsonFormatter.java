cc04f7a2a3e7cafb112dcc1b261932ce75ca569a
Frank <sp42@qq.com>
2023-09-19 10:19:41 +0800

    ci

---
+++ aj-backend/aj-framework/aj-framework/src/main/java/com/ajaxjs/util/logger/JsonFormatter.java
@@ -0,0 +1,25 @@
+package com.ajaxjs.util.logger;
+
+import com.ajaxjs.util.ObjectHelper;
+import com.ajaxjs.util.map.JsonHelper;
+
+import java.util.Map;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class JsonFormatter extends Formatter {
+    @Override
+    public String format(LogRecord record) {
+        // 设置日志记录的级别、消息和时间戳等信息
+        Map<String, Object> map = ObjectHelper.hashMap("level", record.getLevel().getName(), "message", formatMessage(record), "timestamp", record.getMillis());
+
+        // 获取异常信息，如果有的话
+        Throwable throwable = record.getThrown();
+
+        if (throwable != null)
+            map.put("exception", throwable.toString());
+
+        // 返回格式化后的JSON字符串
+        return JsonHelper.toJson(map) + System.lineSeparator();
+    }
+}
---
+++ aj-backend/aj-framework/aj-framework/src/main/java/com/ajaxjs/util/logger/JsonHandler.java
@@ -0,0 +1,22 @@
+package com.ajaxjs.util.logger;
+
+import java.util.logging.*;
+
+public class JsonHandler extends Handler {
+    @Override
+    public void publish(LogRecord record) {
+        // 格式化日志记录
+        String formattedLog = getFormatter().format(record);
+
+        // 将日志输出到控制台或其他目标
+        System.out.print(formattedLog);
+    }
+
+    @Override
+    public void flush() {
+    }
+
+    @Override
+    public void close() throws SecurityException {
+    }
+}
