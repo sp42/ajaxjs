107e266d5efa5e6cbaac772c90e3d21ef3e73b57
Frank <sp42@qq.com>
2024-01-30 12:05:06 +0800

    ci

---
+++ aj-backend/aj-iam/aj-iam-server/src/test/java/com/ajaxjs/iam/permission/TestPermissionControl.java
@@ -0,0 +1,15 @@
+package com.ajaxjs.iam;
+
+public class Test {
+    public static void main(String[] args) {
+        long value = 1230L; // 64-bit long value
+        String binaryString = Long.toBinaryString(value);
+        System.out.println(binaryString);
+
+        10011001110
+
+        System.out.println("1001001100101100000001011010010".length());
+    }
+
+
+}
