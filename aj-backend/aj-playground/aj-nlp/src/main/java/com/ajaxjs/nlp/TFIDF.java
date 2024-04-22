247a49c1af6630463d6b1bce75918b34d4ec8da5
sp42 <sp42@qq.com>
2024-04-22 11:53:25 +0800

    ci

---
+++ aj-backend/aj-playground/aj-nlp/src/main/java/com/ajaxjs/nlp/TFIDF.java
@@ -0,0 +1,230 @@
+/**
+ * Author: WuLC
+ * Date:   2016-05-22 17:46:15
+ * Last modified by:   WuLC
+ * Last Modified time: 2016-05-23 23:31:25
+ * Email: liangchaowu5@gmail.com
+ * ***********************************************************
+ * Function:get keywords of file through TF-IDF algorithm
+ * Input: path of directory of files that need to extract keywords
+ * Output: keywords of each file
+ */
+
+package com.ajaxjs.nlp;
+
+import com.ajaxjs.nlp.parsedoc.ReadDir;
+import com.ajaxjs.nlp.parsedoc.ReadFile;
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.dictionary.stopword.CoreStopWordDictionary;
+import com.hankcs.hanlp.seg.common.Term;
+
+import java.util.*;
+
+
+public class TFIDF {
+    private static int keywordsNumber = 5;
+
+    /**
+     * change the number of keywords,default 5
+     *
+     * @param keywordNum(int): number of keywords that need to be extracted
+     */
+    public static void setKeywordsNumber(int keywordNum) {
+        keywordsNumber = keywordNum;
+    }
+
+    /**
+     * calculate TF value of each word in terms of  the content of file
+     *
+     * @param fileContent(String): content of file
+     * @return "words:TF value" pairs
+     */
+    public static HashMap<String, Float> getTF(String fileContent) {
+        List<Term> terms = HanLP.segment(fileContent);
+        ArrayList<String> words = new ArrayList<>();
+
+        for (Term t : terms) {
+            if (TFIDF.shouldInclude(t)) {
+                words.add(t.word);
+            }
+        }
+
+        // get TF values
+        HashMap<String, Integer> wordCount = new HashMap<>();
+        HashMap<String, Float> TFValues = new HashMap<>();
+
+        for (String word : words) {
+            if (wordCount.get(word) == null) wordCount.put(word, 1);
+            else wordCount.put(word, wordCount.get(word) + 1);
+
+        }
+
+        int wordLen = words.size();
+        //traverse the HashMap
+        Iterator<Map.Entry<String, Integer>> iter = wordCount.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<String, Integer> entry =  iter.next();
+            TFValues.put(entry.getKey().toString(), Float.parseFloat(entry.getValue().toString()) / wordLen);
+            //System.out.println(entry.getKey().toString() + " = "+  Float.parseFloat(entry.getValue().toString()) / wordLen);
+        }
+
+        return TFValues;
+    }
+
+    /**
+     * judge whether a word belongs to stop words
+     *
+     * @param term(Term): word needed to be judged
+     * @return if the word is a stop word,return false;otherwise return true
+     */
+    public static boolean shouldInclude(Term term) {
+        return CoreStopWordDictionary.shouldInclude(term);
+    }
+
+    /**
+     * calculate TF values for each word of each file under a directory
+     *
+     * @param dirPath(String): path of the directory
+     * @return path of file and its  corresponding "word-TF Value" pairs
+     */
+    public static HashMap<String, HashMap<String, Float>> tfForDir(String dirPath) {
+        HashMap<String, HashMap<String, Float>> allTF = new HashMap<>();
+        List<String> filelist = ReadDir.readDirFileNames(dirPath);
+
+        for (String file : filelist) {
+            String content = ReadFile.loadFile(file); // remember to modify the loadFile method of class ReadFile
+            HashMap<String, Float> dict = TFIDF.getTF(content);
+            allTF.put(file, dict);
+        }
+
+        return allTF;
+    }
+
+    /**
+     * calculate IDF values for each word  under a directory
+     *
+     * @param dirPath(String): path of the directory
+     * @return "word:IDF Value" pairs
+     */
+    public static HashMap<String, Float> idfForDir(String dirPath) {
+        List<String> fileList = ReadDir.readDirFileNames(dirPath);
+        int docNum = fileList.size();
+        Map<String, Set<String>> passageWords = new HashMap<>();
+
+        // get words that are not repeated of a file 
+        for (String filePath : fileList) {
+            List<Term> terms = HanLP.segment(content);
+            Set<String> words = new HashSet<>();
+            String content = ReadFile.loadFile(filePath); // remember to modify the loadFile method of class ReadFile   
+
+            for (Term t : terms) {
+                if (TFIDF.shouldInclude(t)) {
+                    words.add(t.word);
+                }
+            }
+
+            passageWords.put(filePath, words);
+        }
+
+        // get IDF values
+        HashMap<String, Integer> wordPassageNum = new HashMap<>();
+        for (String filePath : fileList) {
+            Set<String> wordSet = passageWords.get(filePath);
+
+            for (String word : wordSet) {
+                if (wordPassageNum.get(word) == null) wordPassageNum.put(word, 1);
+                else wordPassageNum.put(word, wordPassageNum.get(word) + 1);
+            }
+        }
+
+        HashMap<String, Float> wordIDF = new HashMap<String, Float>();
+        Iterator<Map.Entry<String, Integer>> iter_dict = wordPassageNum.entrySet().iterator();
+        while (iter_dict.hasNext()) {
+            Map.Entry<String, Integer> entry = (Map.Entry<String, Integer>) iter_dict.next();
+            float value = (float) Math.log(docNum / (Float.parseFloat(entry.getValue().toString())));
+            wordIDF.put(entry.getKey().toString(), value);
+            //System.out.println(entry.getKey().toString() + "=" +value);
+        }
+
+        return wordIDF;
+    }
+
+
+    /**
+     * calculate TF-IDF value for each word of each file under a directory
+     *
+     * @param dirPath(String): path of the directory
+     * @return path of file and its corresponding "word:TF-IDF Value" pairs
+     */
+    public static Map<String, HashMap<String, Float>> getDirTFIDF(String dirPath) {
+        HashMap<String, HashMap<String, Float>> dirFilesTF = TFIDF.tfForDir(dirPath);
+        HashMap<String, Float> dirFilesIDF = TFIDF.idfForDir(dirPath);
+
+        Map<String, HashMap<String, Float>> dirFilesTFIDF = new HashMap<>();
+        Map<String, Float> singlePassageWord;
+        List<String> fileList = ReadDir.readDirFileNames(dirPath);
+
+        for (String filePath : fileList) {
+            HashMap<String, Float> temp = new HashMap<>();
+            singlePassageWord = dirFilesTF.get(filePath);
+            Iterator<Map.Entry<String, Float>> it = singlePassageWord.entrySet().iterator();
+
+            while (it.hasNext()) {
+                Map.Entry<String, Float> entry = it.next();
+                String word = entry.getKey();
+                Float TFIDF = entry.getValue() * dirFilesIDF.get(word);
+                temp.put(word, TFIDF);
+            }
+
+            dirFilesTFIDF.put(filePath, temp);
+        }
+
+        return dirFilesTFIDF;
+    }
+
+
+    /**
+     * get keywords of each file under a certain directory
+     *
+     * @param dirPath(String): path of directory
+     *                         //     * @param keywordNum(int): number of keywords to extract
+     * @return path of file and its corresponding keywords
+     */
+    public static Map<String, List<String>> getKeywords(String dirPath) {
+        List<String> fileList = ReadDir.readDirFileNames(dirPath);
+
+        // calculate TF-IDF value for each word of each file under the dirPath
+        Map<String, HashMap<String, Float>> dirTFIDF;
+        dirTFIDF = TFIDF.getDirTFIDF(dirPath);
+
+        Map<String, List<String>> keywordsForDir = new HashMap<>();
+        for (String file : fileList) {
+            Map<String, Float> singlePassageTFIDF;
+            singlePassageTFIDF = dirTFIDF.get(file);
+
+            //sort the keywords in terms of TF-IDF value in descending order
+            List<Map.Entry<String, Float>> entryList = new ArrayList<Map.Entry<String, Float>>(singlePassageTFIDF.entrySet());
+
+            Collections.sort(entryList, new Comparator<Map.Entry<String, Float>>() {
+                @Override
+                public int compare(Map.Entry<String, Float> c1, Map.Entry<String, Float> c2) {
+                    return c2.getValue().compareTo(c1.getValue());
+                }
+            });
+
+            // get keywords
+            List<String> systemKeywordList = new ArrayList<String>();
+            for (int k = 0; k < keywordsNumber; k++) {
+                try {
+                    systemKeywordList.add(entryList.get(k).getKey());
+                } catch (IndexOutOfBoundsException e) {
+                    continue;
+                }
+            }
+
+            keywordsForDir.put(file, systemKeywordList);
+        }
+        return keywordsForDir;
+    }
+
+}
